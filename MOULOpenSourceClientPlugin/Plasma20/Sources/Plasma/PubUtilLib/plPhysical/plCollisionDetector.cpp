2c5e01464ee03ea31cd2cdd6af4b7215e763e9bd
Branan Purvine-Riley <branan@gmail.com>
2011-04-10 23:20:08 -0700

    STL fixes needed to link from StartUp to Personal

--- MOULOpenSourceClientPlugin/Plasma20/Sources/Plasma/PubUtilLib/plPhysical/plCollisionDetector.cpp
+++ MOULOpenSourceClientPlugin/Plasma20/Sources/Plasma/PubUtilLib/plPhysical/plCollisionDetector.cpp
@@ -624,7 +624,7 @@ void plObjectInVolumeDetector::IHandleEval(plEvalMsg* pEval)
 {
 		plgDispatch::Dispatch()->UnRegisterForExactType(plEvalMsg::Index(), GetKey());
 		fWaitingForEval = false;
-		for(bookKeepingList::iterator it= (--fCollisionList.end());it!=(--fCollisionList.begin()); it--)
+		for(bookKeepingList::iterator it= fCollisionList.begin();it!=fCollisionList.end(); it++)
 		{
 			bool alreadyInside;
 			ResidentSet::iterator HitIt;
