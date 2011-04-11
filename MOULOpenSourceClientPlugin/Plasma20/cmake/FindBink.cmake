bdd5b7802d37084a9beaa3713385db21593df773
Michael Hansen <zrax0111@gmail.com>
2011-04-11 01:38:01 -0700

    Fix Bink dependencies if the Bink SDK isn't found

--- MOULOpenSourceClientPlugin/Plasma20/cmake/FindBink.cmake
+++ MOULOpenSourceClientPlugin/Plasma20/cmake/FindBink.cmake
@@ -1,9 +1,3 @@
-option(Bink_SDK_AVAILABLE "Do we have the Bink SDK?" OFF)
-
-if(Bink_SDK_AVAILABLE)
-    add_definitions(-DBINK_SDK_AVAILABLE)
-endif(Bink_SDK_AVAILABLE)
-
 find_path(Bink_INCLUDE_DIR bink.h
     C:/BinkW32
 )
@@ -11,5 +5,12 @@ find_path(Bink_INCLUDE_DIR bink.h
 find_library(Bink_LIBRARY NAMES binkw32
     PATHS C:/BinkW32
 )
+
+if(Bink_INCLUDE_DIR AND Bink_LIBRARY)
+    set(Bink_SDK_AVAILABLE TRUE)
+    add_definitions(-DBINK_SDK_AVAILABLE)
+else()
+    set(Bink_SDK_AVAILABLE FALSE)
+endif()
 
 set(Bink_LIBRARIES ${Bink_LIBRARY})
