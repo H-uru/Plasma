498dead64dc0b46bf24055980ba997ff2dfa05b4
Michael Hansen <zrax0111@gmail.com>
2011-04-11 01:44:53 -0700

    Don't change the iteration order

--- MOULOpenSourceClientPlugin/Plasma20/Sources/Plasma/PubUtilLib/plPhysical/plCollisionDetector.cpp
+++ MOULOpenSourceClientPlugin/Plasma20/Sources/Plasma/PubUtilLib/plPhysical/plCollisionDetector.cpp
@@ -624,7 +624,7 @@ void plObjectInVolumeDetector::IHandleEval(plEvalMsg* pEval)
 {
 		plgDispatch::Dispatch()->UnRegisterForExactType(plEvalMsg::Index(), GetKey());
 		fWaitingForEval = false;
-		for(bookKeepingList::iterator it= fCollisionList.begin();it!=fCollisionList.end(); it++)
+		for(bookKeepingList::reverse_iterator it= fCollisionList.rbegin();it!=fCollisionList.rend(); it++)
 		{
 			bool alreadyInside;
 			ResidentSet::iterator HitIt;
