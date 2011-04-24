32cb43d1e005650a1eb4c120961b905e86545177
Darryl Pogue <dvpdiner2@gmail.com>
2011-04-24 13:46:28 -0700

    Fix some include directives

--- Sources/Plasma/PubUtilLib/plAudioCore/plOGGCodec.cpp
+++ Sources/Plasma/PubUtilLib/plAudioCore/plOGGCodec.cpp
@@ -33,9 +33,9 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 //                                                                          //
 //////////////////////////////////////////////////////////////////////////////
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
 
 #include <math.h>
 #include <vorbis/codec.h>
