b6a371f49fe242ba1da6a74fb60e713a48c5d928
Florian Meißner <developer@mystler.eu>
2011-11-30 15:20:12 +0100

    Add animation loop functions to Python

--- Sources/Plasma/FeatureLib/pfPython/cyAvatar.cpp
+++ Sources/Plasma/FeatureLib/pfPython/cyAvatar.cpp
@@ -1761,6 +1761,44 @@ bool cyAvatar::ExitPBMode()
     return IExitTopmostGenericMode();
 }
 
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Function   : EnterAnimMode
+//  PARAMETERS : animName - string
+//
+//  PURPOSE    : Makes the avatar enter a custom anim loop.
+//
+void cyAvatar::EnterAnimMode(plString animName)
+{
+    plArmatureMod *fAvMod = plAvatarMgr::GetInstance()->GetLocalAvatar();
+    if (!fAvMod->FindAnimInstance(animName)) {
+        plKey avKey = fAvMod->GetKey();
+        plAvAnimTask *animTask = new plAvAnimTask(animName, 0.0, 1.0, 1.0, 0.0, true, true, true);
+        plAvTaskMsg *taskMsg = new plAvTaskMsg(avKey, avKey, animTask);
+        taskMsg->SetBCastFlag(plMessage::kNetPropagate);
+        taskMsg->Send();
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Function   : ExitAnimMode
+//  PARAMETERS : animName - string
+//
+//  PURPOSE    : Makes the avatar stop the custom anim loop.
+//
+void cyAvatar::ExitAnimMode(plString animName)
+{
+    plArmatureMod *fAvMod = plAvatarMgr::GetInstance()->GetLocalAvatar();
+    if (fAvMod->FindAnimInstance(animName)) {
+        plKey avKey = fAvMod->GetKey();
+        plAvAnimTask *animTask = new plAvAnimTask(animName, -1.0);
+        plAvTaskMsg *taskMsg = new plAvTaskMsg(avKey, avKey, animTask);
+        taskMsg->SetBCastFlag(plMessage::kNetPropagate);
+        taskMsg->Send();
+    }
+}
+
 
 int cyAvatar::GetCurrentMode()
 {
