8e79583717720e218197e890eba0dad141cdc5e2
Adam Johnson <AdamJohnso@gmail.com>
2012-03-06 22:34:59 -0500

    Add some Python Helpers
    We can now convert plString to PyUnicode objects and have static methods
    with variable arguments and variable keyword arguments

--- Sources/Plasma/FeatureLib/pfPython/pyGlueHelpers.cpp
+++ Sources/Plasma/FeatureLib/pfPython/pyGlueHelpers.cpp
@@ -62,3 +62,9 @@ bool PyString_CheckEx(PyObject* obj)
 {
     return (PyString_Check(obj) || PyUnicode_Check(obj));
 }
+
+PyObject* PyUnicode_FromStringEx(const plString& str)
+{
+    plStringBuffer<wchar_t> buf = str.ToWchar();
+    return PyUnicode_FromWideChar(buf.GetData(), buf.GetSize());
+}
