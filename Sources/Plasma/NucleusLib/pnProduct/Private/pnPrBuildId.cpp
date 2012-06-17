e7fda23cf8f03a66d17d0856fb4f4dfec7cb69e9
CyanWorlds <none@cyanworlds.com>
2012-06-16 23:04:19 -0400

    Build 912

--- Sources/Plasma/NucleusLib/pnProduct/Private/pnPrBuildId.cpp
+++ Sources/Plasma/NucleusLib/pnProduct/Private/pnPrBuildId.cpp
@@ -57,7 +57,7 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 
 // This line must NEVER be modified manually; it is automatically updated
 // by the build server.
-#define BUILD_ID 902
+#define BUILD_ID 912
 COMPILER_ASSERT(BUILD_ID != 0);
 
 
