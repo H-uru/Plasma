3ea82ca589726c05e46fbee60f398f013d7216ab
Adam Johnson <AdamJohnso@gmail.com>
2012-11-18 00:29:43 -0500

    Be smarter about Pch.[h|cpp] files
    As pointed out in #225, Pch.cpp looks suspiciously like a garbage file
    when precompiled headers are disabled in cmake. To solve that, I have
    changed the precompiled header function to add the Pch source files only
    when pch is enabled. This should prevent future accidents.

--- cmake/PrecompiledHeader.cmake
+++ cmake/PrecompiledHeader.cmake
@@ -8,7 +8,7 @@ if(PCH_SUPPORTED)
     option(PLASMA_USE_PCH "Enable precompiled headers?" ON)
 endif(PCH_SUPPORTED)
 
-macro(use_precompiled_header PrecompiledHeader PrecompiledSource)
+function(use_precompiled_header PrecompiledHeader PrecompiledSource Headers Sources)
     if(PLASMA_USE_PCH)
         if(MSVC)
             get_filename_component(PrecompiledBasename ${PrecompiledHeader} NAME_WE)
@@ -19,5 +19,10 @@ macro(use_precompiled_header PrecompiledHeader PrecompiledSource)
             add_definitions(/FI"${PrecompiledHeader}")
             set_source_files_properties(${PrecompiledSource} PROPERTIES COMPILE_FLAGS "/Yc\"${PrecompiledHeader}\"")
         endif(MSVC)
+
+        # Add the Pch.[h|cpp] to the appropriate sets
+        # OT: This has to be the oddest thing I've ever written =/
+        set(${Headers} ${${Headers}} ${PrecompiledHeader} PARENT_SCOPE)
+        set(${Sources} ${${Sources}} ${PrecompiledSource} PARENT_SCOPE)
     endif(PLASMA_USE_PCH)
-endmacro(use_precompiled_header)
+endfunction(use_precompiled_header)
