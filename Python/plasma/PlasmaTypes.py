H-uru/Plasma

commit 65da644f07d4a9a0789173980760a55377542bb1

Convert old style exceptions
Exceptions were backported from Python3 in Python 2.6. The old style
doesn't work in Python 2.7, so let's not have doubly-exploding errors.

Python/plasma/PlasmaTypes.py
@@ -223,10 +223,11 @@ def PtFindAvatar(events):
 ####################################
 # Exceptions
 ####################################
-import exceptions
-class ptResponderStateError(exceptions.Exception):
+class ptResponderStateError(Exception):
     def __init__(self,args=None):
         self.args = args
+    def __str__(self):
+        return repr(self.args)
 
 #
 # Attributes that will be exposed in Max to be filled in by <someone>

Python/xEnum.py
@@ -44,10 +44,13 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 # Written by Will Ware, 2001/08/23
 # Modified by Adam Van Ornum, 4/17/2003
 
-import types, string, exceptions
+import types, string
 
-class EnumException(exceptions.Exception):
-    pass
+class EnumException(Exception):
+    def __init__(self, value):
+        self._value
+    def __str__(self):
+        return repr(self._value)
 
 class Enum:
     def __init__(self, enumStr):
@@ -66,13 +69,13 @@ class Enum:
             if type(x) == types.TupleType:
                 x, i = x
             if type(x) != types.StringType:
-                raise EnumException, "enum name is not a string: " + x
+                raise EnumException("enum name is not a string: " + x)
             if type(i) != types.IntType:
-                raise EnumException, "enum value is not an integer: " + i
+                raise EnumException("enum value is not an integer: " + i)
             if x in uniqueNames:
-                raise EnumException, "enum name is not unique: " + x
+                raise EnumException("enum name is not unique: " + x)
             if i in uniqueValues:
-                raise EnumException, "enum value is not unique for " + x
+                raise EnumException("enum value is not unique for " + x)
             uniqueNames.append(x)
             uniqueValues.append(i)
             lookup[x] = i

Python/bhroBahroYeeshaCave.py
@@ -482,7 +482,7 @@ class bhroBahroYeeshaCave(ptModifier):
             node.chronicleSetValue(varstr)
             node.save()
         else:
-            raise "Could not find chronicle variable to set"
+            raise RuntimeError("Could not find chronicle variable to set")
 
 
     def AreListsEquiv(self, list1, list2):

Python/xAgeSDLIntStateListResp.py
@@ -87,7 +87,7 @@ class ptAttribStateResponder(ptAttribResponder):
             if type(state) == type(0) and state >= 0:
                 nt.addResponderState(state)
             else:
-                raise ptResponderStateError,"State must be a positive integer"
+                raise ptResponderStateError("State must be a positive integer")
             
             # see if there are events to pass on
             if type(events) != type(None):

Python/xPsnlVaultSDL.py
@@ -81,7 +81,7 @@ class xPsnlVaultSDL:
         if type(val) == type( () ):
             val = val[0]
         else:
-            raise "Value must be tuple type"
+            raise ValueError("Value must be tuple type")
         
         if 1:#try:
             if self.useAgeVault:

Python/xRandom.py
@@ -73,7 +73,7 @@ def randint(start, stop):
                 newInt = random.randint(start, stop)
                 iter = iter + 1
             if newInt == _lastvalue and iter >= _MAX_ITERATIONS:
-                raise "Problem with randomness: over max series length and can't find a new number"
+                raise RuntimeError("Problem with randomness: over max series length and can't find a new number")
             _lastvalue = newInt
             _series_length = 1
         else:

commit 68053a74680ec814f32c1ef434f65d83a9b36108

Fix issues introduced in 65da644f07

Python/plasma/PlasmaTypes.py
@@ -225,9 +225,7 @@ def PtFindAvatar(events):
 ####################################
 class ptResponderStateError(Exception):
     def __init__(self,args=None):
-        self.args = args
-    def __str__(self):
-        return repr(self.args)
+        Exception.__init__(self, args)
 
 #
 # Attributes that will be exposed in Max to be filled in by <someone>

Python/xEnum.py
@@ -48,9 +48,7 @@ import types, string
 
 class EnumException(Exception):
     def __init__(self, value):
-        self._value
-    def __str__(self):
-        return repr(self._value)
+        Exception.__init__(self, value)
 
 class Enum:
     def __init__(self, enumStr):