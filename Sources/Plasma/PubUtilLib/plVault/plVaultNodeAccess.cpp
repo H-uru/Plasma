H-uru/Plasma

commit 49dfb4e54684facc5396974a34b73a9e532cf09a

Convert SDL (mostly) to plStrings

Sources/Plasma/PubUtilLib/plVault/plVaultNodeAccess.cpp
@@ -534,13 +534,12 @@ bool VaultSDLNode::GetStateDataRecord (plStateDataRecord * rec, unsigned readOpt
     ram.Write(sdlDataLen, sdlData);
     ram.Rewind();
     
-    char *  sdlRecName = nil;
-    int     sdlRecVersion;
+    plString sdlRecName;
+    int sdlRecVersion;
     if (!plStateDataRecord::ReadStreamHeader(&ram, &sdlRecName, &sdlRecVersion))
         return false;
         
     rec->SetDescriptor(sdlRecName, sdlRecVersion);
-    free(sdlRecName);
 
     // Note: Setting from default here results in a bug causing age SDL to
     // be incorrectly shown when immediately linking back to an age you linked
@@ -698,14 +697,14 @@ bool VaultImageNode::ExtractImage (plMipmap ** dst) {
 #ifdef CLIENT
 struct MatchesSpawnPointTitle
 {
-    std::string fTitle;
-    MatchesSpawnPointTitle( const char * title ):fTitle( title ){}
+    plString fTitle;
+    MatchesSpawnPointTitle( const plString & title ):fTitle( title ){}
     bool operator ()( const plSpawnPointInfo & p ) const { return ( p.fTitle==fTitle ); }
 };
 struct MatchesSpawnPointName
 {
-    std::string fName;
-    MatchesSpawnPointName( const char * name ):fName( name ){}
+    plString fName;
+    MatchesSpawnPointName( const plString & name ):fName( name ){}
     bool operator ()( const plSpawnPointInfo & p ) const { return ( p.fSpawnPt==fName ); }
 };
 #endif
@@ -768,7 +767,7 @@ void VaultAgeLinkNode::AddSpawnPoint (const plSpawnPointInfo & point) {
 
 //============================================================================
 #ifdef CLIENT
-void VaultAgeLinkNode::RemoveSpawnPoint (const char spawnPtName[]) {
+void VaultAgeLinkNode::RemoveSpawnPoint (const plString & spawnPtName) {
 
     plSpawnPointVec points;
     GetSpawnPoints( &points );                                                  
@@ -784,7 +783,7 @@ void VaultAgeLinkNode::RemoveSpawnPoint (const char spawnPtName[]) {
 
 //============================================================================
 #ifdef CLIENT
-bool VaultAgeLinkNode::HasSpawnPoint (const char spawnPtName[]) const {
+bool VaultAgeLinkNode::HasSpawnPoint (const plString & spawnPtName) const {
 
     plSpawnPointVec points;
     GetSpawnPoints( &points );                                                  
@@ -804,23 +803,18 @@ bool VaultAgeLinkNode::HasSpawnPoint (const plSpawnPointInfo & point) const {
 #ifdef CLIENT
 void VaultAgeLinkNode::GetSpawnPoints (plSpawnPointVec * out) const {
     
-    char str[2048];
-    memset(&str, 0, sizeof(str));
-    memcpy(str, spawnPoints, min(spawnPointsLen, arrsize(str) - 1));
-    
-    char token1[ 1024 ];
-    hsStringTokenizer izer1( str, ";" );
-    while ( izer1.Next( token1, sizeof( token1 ) ) )
+    plString str = plString::FromUtf8(reinterpret_cast<const char*>(spawnPoints), spawnPointsLen);
+    std::vector<plString> izer = str.Tokenize(";");
+    for (auto token1 = izer.begin(); token1 != izer.end(); ++token1)
     {
         plSpawnPointInfo point;
-        char token2[ 1024 ];
-        hsStringTokenizer izer2( token1, ":" );
-        if ( izer2.Next( token2, sizeof( token2 ) ) )
-            point.fTitle = token2;
-        if ( izer2.Next( token2, sizeof( token2 ) ) )
-            point.fSpawnPt = token2;
-        if ( izer2.Next( token2, sizeof( token2 ) ) )
-            point.fCameraStack = token2;
+        std::vector<plString> izer2 = token1->Tokenize(":");
+        if ( izer2.size() > 0)
+            point.fTitle = izer2[0];
+        if ( izer2.size() > 1)
+            point.fSpawnPt = izer2[1];
+        if ( izer2.size() > 2)
+            point.fSpawnPt = izer2[2];
 
         out->push_back(point);
     }
@@ -831,7 +825,7 @@ void VaultAgeLinkNode::GetSpawnPoints (plSpawnPointVec * out) const {
 #ifdef CLIENT
 void VaultAgeLinkNode::SetSpawnPoints (const plSpawnPointVec & in) {
 
-    std::stringstream ss;
+    plStringStream ss;
     for ( unsigned i=0; i<in.size(); i++ ) {
         ss
             << in[i].fTitle << ":"
@@ -843,8 +837,8 @@ void VaultAgeLinkNode::SetSpawnPoints (const plSpawnPointVec & in) {
         base,
         &spawnPoints,
         &spawnPointsLen,
-        (const uint8_t *)ss.str().c_str(),
-        ss.str().size()
+        (const uint8_t *)ss.GetString().c_str(),
+        ss.GetLength()
     );
 }
 #endif

Sources/Plasma/CoreLib/plString.cpp
@@ -709,7 +709,7 @@ static bool ch_in_set(char ch, const char *set)
     return false;
 }
 
-std::vector<plString> plString::Tokenize(const char *delims)
+std::vector<plString> plString::Tokenize(const char *delims) const
 {
     std::vector<plString> result;
 
@@ -733,7 +733,7 @@ std::vector<plString> plString::Tokenize(const char *delims)
 }
 
 //TODO: Not binary safe
-std::vector<plString> plString::Split(const char *split, size_t maxSplits)
+std::vector<plString> plString::Split(const char *split, size_t maxSplits) const
 {
     std::vector<plString> result;
 
@@ -760,6 +760,30 @@ plString operator+(const plString &left, const plString &right)
     char *catstr = cat.fUtf8Buffer.CreateWritableBuffer(left.GetSize() + right.GetSize());
     memcpy(catstr, left.c_str(), left.GetSize());
     memcpy(catstr + left.GetSize(), right.c_str(), right.GetSize());
+    catstr[cat.fUtf8Buffer.GetSize()] = 0;
+
+    return cat;
+}
+
+plString operator+(const plString &left, const char *right)
+{
+    plString cat;
+    size_t rsize = strlen(right);
+    char *catstr = cat.fUtf8Buffer.CreateWritableBuffer(left.GetSize() + rsize);
+    memcpy(catstr, left.c_str(), left.GetSize());
+    memcpy(catstr + left.GetSize(), right, rsize);
+    catstr[cat.fUtf8Buffer.GetSize()] = 0;
+
+    return cat;
+}
+
+plString operator+(const char *left, const plString &right)
+{
+    plString cat;
+    size_t lsize = strlen(left);
+    char *catstr = cat.fUtf8Buffer.CreateWritableBuffer(lsize + right.GetSize());
+    memcpy(catstr, left, lsize);
+    memcpy(catstr + lsize, right.c_str(), right.GetSize());
     catstr[cat.fUtf8Buffer.GetSize()] = 0;
 
     return cat;

Sources/Plasma/CoreLib/plString.h
@@ -163,18 +163,15 @@ private:
 public:
     plString() { }
 
-#ifndef PLSTRING_POLLUTE_ASCII_CAST
     plString(const char *cstr) { IConvertFromUtf8(cstr, kSizeAuto); }
-#endif
     plString(const plString &copy) : fUtf8Buffer(copy.fUtf8Buffer) { }
     plString(const plStringBuffer<char> &init) { operator=(init); }
 
-#ifndef PLSTRING_POLLUTE_ASCII_CAST
     plString &operator=(const char *cstr) { IConvertFromUtf8(cstr, kSizeAuto); return *this; }
-#endif
     plString &operator=(const plString &copy) { fUtf8Buffer = copy.fUtf8Buffer; return *this; }
     plString &operator=(const plStringBuffer<char> &init);
 
+    plString &operator+=(const char *cstr) { return operator=(*this + cstr); }
     plString &operator+=(const plString &str) { return operator=(*this + str); }
 
     static inline plString FromUtf8(const char *utf8, size_t size = kSizeAuto)
@@ -205,10 +202,8 @@ public:
         return str;
     }
 
-#ifndef PLSTRING_POLLUTE_C_STR
     const char *c_str(const char *substitute = "") const
     { return IsEmpty() ? substitute : fUtf8Buffer.GetData(); }
-#endif
 
     char CharAt(size_t position) const { return c_str()[position]; }
 
@@ -263,8 +258,15 @@ public:
                                          : strnicmp(c_str(), str, count);
     }
 
+    int CompareI(const plString &str) const { return Compare(str, kCaseInsensitive); }
+    int CompareI(const char *str) const { return Compare(str, kCaseInsensitive); }
+    int CompareNI(const plString &str, size_t count) const { return CompareN(str, count, kCaseInsensitive); }
+    int CompareNI(const char *str, size_t count) const { return CompareN(str, count, kCaseInsensitive); }
+
     bool operator<(const plString &other) const { return Compare(other) < 0; }
+    bool operator==(const char *other) const { return Compare(other) == 0; }
     bool operator==(const plString &other) const { return Compare(other) == 0; }
+    bool operator!=(const char *other) const { return Compare(other) != 0; }
     bool operator!=(const plString &other) const { return Compare(other) != 0; }
 
     int Find(char ch, CaseSensitivity sense = kCaseSensitive) const;
@@ -291,8 +293,8 @@ public:
     // and Tokenize is that Tokenize never returns a blank string (it strips
     // all delimiters and only returns the pieces left between them), whereas
     // Split will split on a full string, returning whatever is left between.
-    std::vector<plString> Split(const char *split, size_t maxSplits = kSizeAuto);
-    std::vector<plString> Tokenize(const char *delims = " \t\r\n\f\v");
+    std::vector<plString> Split(const char *split, size_t maxSplits = kSizeAuto) const;
+    std::vector<plString> Tokenize(const char *delims = " \t\r\n\f\v") const;
 
 public:
     struct less
@@ -423,9 +425,13 @@ public:
 
 private:
     friend plString operator+(const plString &left, const plString &right);
+    friend plString operator+(const plString &left, const char *right);
+    friend plString operator+(const char *left, const plString &right);
 };
 
 plString operator+(const plString &left, const plString &right);
+plString operator+(const plString &left, const char *right);
+plString operator+(const char *left, const plString &right);
 
 
 class plStringStream

Sources/Plasma/FeatureLib/pfConsole/pfConsoleCommands.cpp
@@ -6044,22 +6044,18 @@ PF_CONSOLE_CMD(Age, ShowSDL, "", "Prints the age SDL values")
         return;
     }
     
-    char line[2048];
-
     plStatusLog::AddLineS("ShowSDL.log", "-----------------------------------");
     for (unsigned i = 0; i < rec->GetNumVars(); ++i) {
         plStateVariable * var = rec->GetVar(i);
         if (plSimpleStateVariable * simple = var->GetAsSimpleStateVar()) {
-            const char * name = var->GetName();
-            StrPrintf(line, arrsize(line), "%s=", name);
+            plString line = var->GetName();
+            line += "=";
             for (unsigned j = 0; j < simple->GetCount(); ++j) {
-                char * str = simple->GetAsString(j);
-                StrPack(line, str, arrsize(line));
-                StrPack(line, ",", arrsize(line));
-                free(str);
+                line += simple->GetAsString(j);
+                line += ",";
             }
-            PrintString(line);
-            plStatusLog::AddLineS("ShowSDL.log", "%s", line);
+            PrintString(line.c_str());
+            plStatusLog::AddLineS("ShowSDL.log", "%s", line.c_str());
         }
     }   
     
@@ -6117,7 +6113,7 @@ PF_CONSOLE_CMD( Age, SetSDLFloat, "string varName, float value, int index", "Set
     if (!sdlMod)
         return;
 
-    plSimpleStateVariable *var = sdlMod->GetStateCache()->FindVar(params[0]);
+    plSimpleStateVariable *var = sdlMod->GetStateCache()->FindVar((const char *)params[0]);
     if (!var)
         return;
 
@@ -6141,7 +6137,7 @@ PF_CONSOLE_CMD( Age, SetSDLInt, "string varName, int value, int index", "Set the
     if (!sdlMod)
         return;
 
-    plSimpleStateVariable *var = sdlMod->GetStateCache()->FindVar(params[0]);
+    plSimpleStateVariable *var = sdlMod->GetStateCache()->FindVar((const char *)params[0]);
     if (!var)
         return;
 
@@ -6164,7 +6160,7 @@ PF_CONSOLE_CMD( Age, SetSDLBool, "string varName, bool value, int index", "Set t
     if (!sdlMod)
         return;
 
-    plSimpleStateVariable *var = sdlMod->GetStateCache()->FindVar(params[0]);
+    plSimpleStateVariable *var = sdlMod->GetStateCache()->FindVar((const char*)params[0]);
     if (!var)
         return;
 

Sources/Plasma/FeatureLib/pfConsole/pfConsoleCommandsNet.cpp
@@ -356,7 +356,7 @@ PF_CONSOLE_CMD( Net,        // groupName
     link.GetAgeInfo()->SetAgeFilename( params[0] );
     //link.GetAgeInfo()->SetAgeInstanceName( params[0] );
     //link.GetAgeInfo()->SetAgeUserDefinedName( params[0] );
-    plUUID guid(params[1]);
+    plUUID guid( (const char *)params[1] );
     link.GetAgeInfo()->SetAgeInstanceGuid( &guid );
     link.SetLinkingRules( plNetCommon::LinkingRules::kBasicLink );
     plNetLinkingMgr::GetInstance()->LinkToAge( &link );
@@ -379,7 +379,7 @@ PF_CONSOLE_CMD( Net,
 {
     plAgeLinkStruct link;
     link.GetAgeInfo()->SetAgeFilename( params[0] );
-    link.SpawnPoint() = plSpawnPointInfo( params[1], params[1] );
+    link.SpawnPoint() = plSpawnPointInfo( (const char *)params[1], (const char *)params[1] );
     link.SetLinkingRules( plNetCommon::LinkingRules::kOriginalBook );
     plNetLinkingMgr::GetInstance()->LinkToAge( &link );
     PrintString("Linking to age with original book...");

Sources/Plasma/FeatureLib/pfPython/plPythonFileMod.cpp
@@ -2300,16 +2300,14 @@ bool plPythonFileMod::MsgReceive(plMessage* msg)
         plSDLNotificationMsg* sn = plSDLNotificationMsg::ConvertNoRef(msg);
         if (sn)
         {
-            const char* tag = sn->fHintString.c_str();
-            if (tag == nil)
-                tag = "";
+            plString tag = sn->fHintString;
             // yes... then call it
             plProfile_BeginTiming(PythonUpdate);
             PyObject* retVal = PyObject_CallMethod(
                     fPyFunctionInstances[kfunc_SDLNotify],
                     (char*)fFunctionNames[kfunc_SDLNotify],
-                    "ssls", sn->fVar->GetName(), sn->fSDLName.c_str(),
-                    sn->fPlayerID, tag);
+                    "ssls", sn->fVar->GetName().c_str(), sn->fSDLName.c_str(),
+                    sn->fPlayerID, tag.c_str());
             if ( retVal == nil )
             {
 #ifndef PLASMA_EXTERNAL_RELEASE

Sources/Plasma/FeatureLib/pfPython/plPythonSDLModifier.cpp
@@ -81,7 +81,7 @@ plPythonSDLModifier::plPythonSDLModifier(plPythonFileMod* owner) : fOwner(owner)
         {
             plVarDescriptor* var = desc->GetVar(i);
 
-            const char* name = var->GetName();
+            plString name = var->GetName();
             int count = var->GetCount();
 
             fMap[name] = SDLObj(nil, count, false);
@@ -99,15 +99,14 @@ plPythonSDLModifier::~plPythonSDLModifier()
     }
 }
 
-PyObject* plPythonSDLModifier::GetItem(const char* key)
+PyObject* plPythonSDLModifier::GetItem(const plString& key)
 {
     SDLMap::iterator it = fMap.find(key);
 
     if (it == fMap.end())
     {
-        char errmsg[256];
-        sprintf(errmsg,"SDL key %s not found",key);
-        PyErr_SetString(PyExc_KeyError, errmsg);
+        plString errmsg = plString::Format("SDL key %s not found", key.c_str());
+        PyErr_SetString(PyExc_KeyError, errmsg.c_str());
         PYTHON_RETURN_ERROR;
     }
 
@@ -119,7 +118,7 @@ PyObject* plPythonSDLModifier::GetItem(const char* key)
     return val;
 }
 
-void plPythonSDLModifier::ISetItem(const char* key, PyObject* value)
+void plPythonSDLModifier::ISetItem(const plString& key, PyObject* value)
 {
     if (!value || !PyTuple_Check(value))
     {
@@ -149,19 +148,19 @@ void plPythonSDLModifier::ISetItem(const char* key, PyObject* value)
     oldObj.obj = value;
 }
 
-void plPythonSDLModifier::SendToClients(const char* key)
+void plPythonSDLModifier::SendToClients(const plString& key)
 {
     SDLMap::iterator it = fMap.find(key);
     if (it != fMap.end())
         it->second.sendToClients = true;
 }
 
-void plPythonSDLModifier::SetNotify(pyKey& selfkey, const char* key, float tolerance)
+void plPythonSDLModifier::SetNotify(pyKey& selfkey, const plString& key, float tolerance)
 {
     AddNotifyForVar(selfkey.getKey(), key, tolerance);
 }
 
-void plPythonSDLModifier::SetItem(const char* key, PyObject* value)
+void plPythonSDLModifier::SetItem(const plString& key, PyObject* value)
 {
     ISetItem(key, value);
     IDirtySynchState(key);
@@ -169,7 +168,7 @@ void plPythonSDLModifier::SetItem(const char* key, PyObject* value)
 
 void plPythonSDLModifier::SetItemFromSDLVar(plSimpleStateVariable* var)
 {
-    const char* name = var->GetName();
+    plString name = var->GetName();
 
     // Get the SDL value in Python format
     PyObject* pyVar = ISDLVarToPython(var);
@@ -180,12 +179,12 @@ void plPythonSDLModifier::SetItemFromSDLVar(plSimpleStateVariable* var)
 }
 
 
-void plPythonSDLModifier::SetDefault(const char* key, PyObject* value)
+void plPythonSDLModifier::SetDefault(const plString& key, PyObject* value)
 {
     ISetItem(key, value);
 }
 
-void plPythonSDLModifier::SetItemIdx(const char* key, int idx, PyObject* value, bool sendImmediate)
+void plPythonSDLModifier::SetItemIdx(const plString& key, int idx, PyObject* value, bool sendImmediate)
 {
     if (!value)
     {
@@ -269,7 +268,7 @@ const char* plPythonSDLModifier::GetSDLName() const
     return fOwner->fPythonFile;
 }
 
-void plPythonSDLModifier::SetFlags(const char* name, bool sendImmediate, bool skipOwnershipCheck)
+void plPythonSDLModifier::SetFlags(const plString& name, bool sendImmediate, bool skipOwnershipCheck)
 {
     SDLMap::iterator it = fMap.find(name);
     if (it != fMap.end())
@@ -279,7 +278,7 @@ void plPythonSDLModifier::SetFlags(const char* name, bool sendImmediate, bool sk
     }
 }
 
-void plPythonSDLModifier::SetTagString(const char* name, const char* tag)
+void plPythonSDLModifier::SetTagString(const plString& name, const plString& tag)
 {
     SDLMap::iterator it = fMap.find(name);
     if (it != fMap.end())
@@ -297,7 +296,7 @@ void plPythonSDLModifier::ISetCurrentStateFrom(const plStateDataRecord* srcState
     {
         plSimpleStateVariable* var = vars[i];
 
-        const char* name = var->GetName();
+        plString name = var->GetName();
 
         // Get the SDL value in Python format
         PyObject* pyVar = ISDLVarToPython(var);
@@ -334,11 +333,11 @@ void plPythonSDLModifier::IPutCurrentStateIn(plStateDataRecord* dstState)
     SDLMap::iterator it = fMap.begin();
     for (; it != fMap.end(); it++)
     {
-        IPythonVarToSDL(dstState, it->first.c_str());
+        IPythonVarToSDL(dstState, it->first);
     }
 }
 
-void plPythonSDLModifier::IDirtySynchState(const char* name, bool sendImmediate)
+void plPythonSDLModifier::IDirtySynchState(const plString& name, bool sendImmediate)
 {
     SDLMap::iterator it = fMap.find(name);
     if (it != fMap.end())
@@ -358,7 +357,7 @@ void plPythonSDLModifier::IDirtySynchState(const char* name, bool sendImmediate)
 }
 
 bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int varIdx, int type, PyObject* pyVar, 
-                                             const char* hintstring)
+                                             const plString& hintstring)
 {
     switch (type)
     {
@@ -370,7 +369,7 @@ bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int var
         {
             int v = PyInt_AsLong(pyVar);
             var->Set(v, varIdx);
-            if (hintstring)
+            if (!hintstring.IsNull())
                 var->GetNotificationInfo().SetHintString(hintstring);
             return true;
         }
@@ -378,7 +377,7 @@ bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int var
         {
             int v = (int)PyLong_AsLong(pyVar);
             var->Set(v, varIdx);
-            if (hintstring)
+            if (!hintstring.IsNull())
                 var->GetNotificationInfo().SetHintString(hintstring);
             return true;
         }
@@ -387,7 +386,7 @@ bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int var
         {
             int v = (int)PyFloat_AsDouble(pyVar);
             var->Set(v, varIdx);
-            if (hintstring)
+            if (!hintstring.IsNull())
                 var->GetNotificationInfo().SetHintString(hintstring);
             return true;
         }
@@ -398,7 +397,7 @@ bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int var
         {
             float v = (float)PyFloat_AsDouble(pyVar);
             var->Set(v, varIdx);
-            if (hintstring)
+            if (!hintstring.IsNull())
                 var->GetNotificationInfo().SetHintString(hintstring);
             return true;
         }
@@ -407,7 +406,7 @@ bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int var
         {
             float v = (float)PyInt_AsLong(pyVar);
             var->Set(v, varIdx);
-            if (hintstring)
+            if (!hintstring.IsNull())
                 var->GetNotificationInfo().SetHintString(hintstring);
             return true;
         }
@@ -418,7 +417,7 @@ bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int var
         {
             char* v = PyString_AsString(pyVar);
             var->Set(v, varIdx);
-            if (hintstring)
+            if (!hintstring.IsNull())
                 var->GetNotificationInfo().SetHintString(hintstring);
         }
         break;
@@ -428,7 +427,7 @@ bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int var
             pyKey* key = PythonInterface::GetpyKeyFromPython(pyVar);
             if ( key )
                 var->Set(key->getKey(),varIdx);
-                if (hintstring)
+                if (!hintstring.IsNull())
                     var->GetNotificationInfo().SetHintString(hintstring);
         }
         break;
@@ -438,7 +437,7 @@ bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int var
         {
             double v = PyFloat_AsDouble(pyVar);
             var->Set(v, varIdx);
-            if (hintstring)
+            if (!hintstring.IsNull())
                 var->GetNotificationInfo().SetHintString(hintstring);
             return true;
         }
@@ -446,7 +445,7 @@ bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int var
         {
             double v = (double)PyInt_AsLong(pyVar);
             var->Set(v, varIdx);
-            if (hintstring)
+            if (!hintstring.IsNull())
                 var->GetNotificationInfo().SetHintString(hintstring);
             return true;
         }
@@ -462,7 +461,7 @@ bool plPythonSDLModifier::IPythonVarIdxToSDL(plSimpleStateVariable* var, int var
     return false;
 }
 
-void plPythonSDLModifier::IPythonVarToSDL(plStateDataRecord* state, const char* name)
+void plPythonSDLModifier::IPythonVarToSDL(plStateDataRecord* state, const plString& name)
 {
     plSimpleStateVariable* var = state->FindVar(name);
     PyObject* pyVar = nil;
@@ -482,7 +481,7 @@ void plPythonSDLModifier::IPythonVarToSDL(plStateDataRecord* state, const char* 
         {
             PyObject* pyVarItem = PyTuple_GetItem(pyVar, i);
             if (pyVarItem)
-                IPythonVarIdxToSDL(var, i, type, pyVarItem,it->second.hintString.c_str());
+                IPythonVarIdxToSDL(var, i, type, pyVarItem,it->second.hintString);
         }
     }
 }

Sources/Plasma/FeatureLib/pfPython/plPythonSDLModifier.h
@@ -70,11 +70,11 @@ protected:
         bool sendToClients;
         bool skipLocalCheck;
         bool sendImmediate;
-        std::string hintString;
+        plString hintString;
         SDLObj() : obj(nil), size(-1), sendToClients(false) {}
         SDLObj(PyObject* obj, int size, bool sendToClients) : obj(obj), size(size), sendToClients(sendToClients) {}
     };
-    typedef std::map<std::string, SDLObj> SDLMap;
+    typedef std::map<plString, SDLObj> SDLMap;
     SDLMap fMap;
     plPythonFileMod* fOwner;
 
@@ -83,11 +83,11 @@ protected:
     PyObject* ISDLVarToPython(plSimpleStateVariable* var);
     PyObject* ISDLVarIdxToPython(plSimpleStateVariable* var, int type, int idx);
 
-    void IPythonVarToSDL(plStateDataRecord* state, const char* name);
-    bool IPythonVarIdxToSDL(plSimpleStateVariable* var, int varIdx, int type, PyObject* pyVar, const char* hintstring);
+    void IPythonVarToSDL(plStateDataRecord* state, const plString& name);
+    bool IPythonVarIdxToSDL(plSimpleStateVariable* var, int varIdx, int type, PyObject* pyVar, const plString& hintstring);
 
-    void ISetItem(const char* key, PyObject* value);
-    void IDirtySynchState(const char* name, bool sendImmediate = false);
+    void ISetItem(const plString& key, PyObject* value);
+    void IDirtySynchState(const plString& name, bool sendImmediate = false);
 
     void IPutCurrentStateIn(plStateDataRecord* dstState);
     void ISetCurrentStateFrom(const plStateDataRecord* srcState);
@@ -106,15 +106,15 @@ public:
     static const plPythonSDLModifier* FindAgeSDL();
     static plKey FindAgeSDLTarget();
 
-    void SetDefault(const char* key, PyObject* value);
-    void SendToClients(const char* key);
-    void SetNotify(pyKey& selfkey, const char* key, float tolerance);
+    void SetDefault(const plString& key, PyObject* value);
+    void SendToClients(const plString& key);
+    void SetNotify(pyKey& selfkey, const plString& key, float tolerance);
 
-    PyObject* GetItem(const char* key);
-    void SetItem(const char* key, PyObject* value);
-    void SetItemIdx(const char* key, int idx, PyObject* value, bool sendImmediate = false);
-    void SetFlags(const char* name, bool sendImmediate, bool skipOwnershipCheck);
-    void SetTagString(const char* name, const char* tag);
+    PyObject* GetItem(const plString& key);
+    void SetItem(const plString& key, PyObject* value);
+    void SetItemIdx(const plString& key, int idx, PyObject* value, bool sendImmediate = false);
+    void SetFlags(const plString& name, bool sendImmediate, bool skipOwnershipCheck);
+    void SetTagString(const plString& name, const plString& tag);
 };
 
 // A wrapper for plPythonSDLModifier that Python uses

commit 8117aa8bccee490507341f414fef1e4eb350aef7

Fix Cloth Links
A typo introduced in 49dfb4e5 caused camera stacks in AgeLink nodes to
overwrite spawn point names. This broke shell and journey cloth links in
the Relto bookshelf.

Sources/Plasma/PubUtilLib/plVault/plVaultNodeAccess.cpp
@@ -814,7 +814,7 @@ void VaultAgeLinkNode::GetSpawnPoints (plSpawnPointVec * out) const {
         if ( izer2.size() > 1)
             point.fSpawnPt = izer2[1];
         if ( izer2.size() > 2)
-            point.fSpawnPt = izer2[2];
+            point.fCameraStack = izer2[2];
 
         out->push_back(point);
     }