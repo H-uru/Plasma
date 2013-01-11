4233727f57ae3f6455895c8a6ed8f464d763d22a
Michael Hansen <zrax0111@gmail.com>
2013-01-10 18:35:05 -0800

    Generate ComCtl manifests for the Win32 GUI executables

--- Sources/Plasma/Apps/plClient/winmain.cpp
+++ Sources/Plasma/Apps/plClient/winmain.cpp
@@ -1564,3 +1564,7 @@ static void GetCryptKey(uint32_t* cryptKey, unsigned numElements)
     }
 }
 
+/* Enable themes in Windows XP and later */
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
