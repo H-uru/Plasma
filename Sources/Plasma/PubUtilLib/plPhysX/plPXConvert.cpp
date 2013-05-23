9634f9b0505f231b808ca6e10bb0b77f949e81fc
Adam Johnson <AdamJohnso@gmail.com>
2013-05-23 00:07:37 -0400

    PhysX conversion code validation at compile-time
    Seriously? Offsetof has been around for a long time, and eap added a
    compiler_assert for his crazy netcode. There's no excuse for that to be
    happening at runtime.

--- Sources/Plasma/PubUtilLib/plPhysX/plPXConvert.cpp
+++ Sources/Plasma/PubUtilLib/plPhysX/plPXConvert.cpp
@@ -39,44 +39,20 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
       Mead, WA   99021
 
 *==LICENSE==*/
+
 #include "plPXConvert.h"
 
-bool plPXConvert::Validate()
-{
-    NxVec3 nxVec;
-    hsVector3 plVec;
+#define CHECK_OFFSET(NxCls, NxMem, hsCls, hsMem) \
+    static_assert(offsetof(NxCls, NxMem) == offsetof(hsCls, hsMem), \
+    "PhysX Conversion Failure: "#NxCls " and " #hsCls " offsets do not match for " #NxMem "!");
 
-    int nxVecXOffset = ((char*)&nxVec.x) - ((char*)&nxVec);
-    int nxVecYOffset = ((char*)&nxVec.y) - ((char*)&nxVec);
-    int nxVecZOffset = ((char*)&nxVec.z) - ((char*)&nxVec);
+CHECK_OFFSET(NxVec3, x, hsScalarTriple, fX);
+CHECK_OFFSET(NxVec3, y, hsScalarTriple, fY);
+CHECK_OFFSET(NxVec3, z, hsScalarTriple, fZ);
 
-    int plVecXOffset = ((char*)&plVec.fX) - ((char*)&plVec);
-    int plVecYOffset = ((char*)&plVec.fY) - ((char*)&plVec);
-    int plVecZOffset = ((char*)&plVec.fZ) - ((char*)&plVec);
+CHECK_OFFSET(NxQuat, x, hsQuat, fX);
+CHECK_OFFSET(NxQuat, y, hsQuat, fY);
+CHECK_OFFSET(NxQuat, z, hsQuat, fZ);
+CHECK_OFFSET(NxQuat, w, hsQuat, fW);
+#undef CHECK_OFFSET
 
-    NxQuat nxQuat;
-    hsQuat plQuat;
-
-    int nxQuatXOffset = ((char*)&nxQuat.x) - ((char*)&nxQuat);
-    int nxQuatYOffset = ((char*)&nxQuat.y) - ((char*)&nxQuat);
-    int nxQuatZOffset = ((char*)&nxQuat.z) - ((char*)&nxQuat);
-    int nxQuatWOffset = ((char*)&nxQuat.w) - ((char*)&nxQuat);
-
-    int plQuatXOffset = ((char*)&plQuat.fX) - ((char*)&plQuat);
-    int plQuatYOffset = ((char*)&plQuat.fY) - ((char*)&plQuat);
-    int plQuatZOffset = ((char*)&plQuat.fZ) - ((char*)&plQuat);
-    int plQuatWOffset = ((char*)&plQuat.fW) - ((char*)&plQuat);
-
-    bool offsetsOK =
-        nxVecXOffset == plVecXOffset &&
-        nxVecYOffset == plVecYOffset &&
-        nxVecZOffset == plVecZOffset &&
-        nxQuatXOffset == plQuatXOffset &&
-        nxQuatYOffset == plQuatYOffset &&
-        nxQuatZOffset == plQuatZOffset &&
-        nxQuatWOffset == plQuatWOffset;
-
-    hsAssert(offsetsOK, "PhysX or Plasma offsets have changed, need to rewrite conversion code");
-
-    return offsetsOK;
-}
