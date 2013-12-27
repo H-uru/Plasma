309294b7341f5d8c45cb52a148884ef562a70219
Florian Meißner <developer@mystler.eu>
2013-12-27 03:20:16 -0800

    Let CMake handle c-ares

---
+++ cmake/FindCARES.cmake
@@ -0,0 +1,40 @@
+# Find the c-ares includes and library
+# This module defines
+#   CARES_INCLUDE_DIR, where to find ares.h, etc.
+#   CARES_LIBRARIES, the libraries needed to use c-ares.
+#   CARES_FOUND, If false, do not try to use c-ares.
+# Also defined, but not for general use are
+# CARES_LIBRARY, where to find the c-ares library.
+
+find_path(CARES_INCLUDE_DIR ares.h
+    /usr/local/include
+    /usr/include
+)
+
+find_library(CARES_LIBRARY
+    NAMES cares libcares
+    PATHS /usr/lib /usr/local/lib
+)
+
+if(CARES_LIBRARY AND CARES_INCLUDE_DIR)
+    set(CARES_LIBRARIES ${CARES_LIBRARY})
+    set(CARES_FOUND "YES")
+else()
+    set(CARES_FOUND "NO")
+endif()
+
+
+if(CARES_FOUND)
+    if(NOT CARES_FIND_QUIETLY)
+        message(STATUS "Found c-ares: ${CARES_LIBRARIES}")
+    endif()
+else()
+    if(CARES_FIND_REQUIRED)
+        message(FATAL_ERROR "Could not find c-ares library")
+    endif()
+endif()
+
+mark_as_advanced(
+    CARES_LIBRARY
+    CARES_INCLUDE_DIR
+)
