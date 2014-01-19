b0bffa0b5d505d5d69b16a161f46188e8f20a920
Florian Meißner <developer@mystler.eu>
2014-01-19 10:06:33 +0100

    Allow setting the G values for the servers in the server.ini Setting these values is optional. If they are not set in the ini file, default values will be used. This way, we don't have to change all our ini files.

--- Sources/Plasma/NucleusLib/pnNetBase/pnNbKeys.h
+++ Sources/Plasma/NucleusLib/pnNetBase/pnNbKeys.h
@@ -47,17 +47,17 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 #include "pnNbConst.h"
 
 // Auth Server
-static const unsigned kAuthDhGValue = 41;
+extern uint32_t kAuthDhGValue;
 extern uint8_t kAuthDhNData[kNetDiffieHellmanKeyBits / 8];
 extern uint8_t kAuthDhXData[kNetDiffieHellmanKeyBits / 8];
 
 // Game Server
-static const unsigned kGameDhGValue = 73;
+extern uint32_t kGameDhGValue;
 extern uint8_t kGameDhNData[kNetDiffieHellmanKeyBits / 8];
 extern uint8_t kGameDhXData[kNetDiffieHellmanKeyBits / 8];
 
 // GateKeeper Server
-static const unsigned kGateKeeperDhGValue = 4;
+extern uint32_t kGateKeeperDhGValue;
 extern uint8_t kGateKeeperDhNData[kNetDiffieHellmanKeyBits / 8];
 extern uint8_t kGateKeeperDhXData[kNetDiffieHellmanKeyBits / 8];
 
