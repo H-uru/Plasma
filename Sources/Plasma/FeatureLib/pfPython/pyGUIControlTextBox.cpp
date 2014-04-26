openuruunofficial/CWE-ou

add python api to set flags
Clean up unwanted changes caused by IDE extension -.-

Reworked implementation

--HG--
branch : textrendering

MOULOpenSourceClientPlugin/Plasma20/Sources/Plasma/FeatureLib/pfPython/pyGUIControlTextBox.cpp
@@ -214,3 +214,33 @@ UInt8 pyGUIControlTextBox::GetJustify()
 	}
 	return pfGUIListText::kLeftJustify;
 }
+
+void pyGUIControlTextBox::SetFontFlags(UInt8 fontFlags)
+{
+	if (fGCkey)
+	{
+		// get the pointer to the modifier
+		pfGUITextBoxMod* ptbmod = pfGUITextBoxMod::ConvertNoRef(fGCkey->ObjectIsLoaded());
+		if (ptbmod)
+		{
+			pfGUIColorScheme* colorscheme = ptbmod->GetColorScheme();
+			colorscheme->fFontFlags = fontFlags;
+			ptbmod->UpdateColorScheme();
+		}
+	}
+}
+
+UInt8 pyGUIControlTextBox::GetFontFlags()
+{
+	if (fGCkey)
+	{
+		// get the pointer to the modifier
+		pfGUITextBoxMod* ptbmod = pfGUITextBoxMod::ConvertNoRef(fGCkey->ObjectIsLoaded());
+		if (ptbmod)
+		{
+			pfGUIColorScheme* colorscheme = ptbmod->GetColorScheme();
+			return colorscheme->fFontFlags;
+		}
+	}
+	return 0;
+}

MOULOpenSourceClientPlugin/Plasma20/Sources/Plasma/FeatureLib/pfPython/pyGUIControlTextBox.h
@@ -88,8 +88,10 @@ public:
 	virtual void	SetForeColor( pyColor& color );
 	virtual void	SetBackColor( pyColor& color );
 	virtual void	SetJustify( UInt8 justify );
+	virtual void	SetFontFlags(UInt8 fontflags);
 	virtual UInt8	GetJustify();
 	virtual PyObject* GetForeColor(); // returns pyColor
+	virtual UInt8 GetFontFlags();
 };
 
 #endif // _pyGUIControlTextBox_h_

H-uru/Plasma

Sources/Plasma/FeatureLib/pfPython/pyGUIControlTextBox.cpp
@@ -212,3 +212,33 @@ uint8_t pyGUIControlTextBox::GetJustify()
     }
     return pfGUIListText::kLeftJustify;
 }
+
+void pyGUIControlTextBox::SetFontFlags(uint8_t fontFlags)
+{
+    if (fGCkey)
+    {
+        // get the pointer to the modifier
+        pfGUITextBoxMod* ptbmod = pfGUITextBoxMod::ConvertNoRef(fGCkey->ObjectIsLoaded());
+        if (ptbmod)
+        {
+            pfGUIColorScheme* colorscheme = ptbmod->GetColorScheme();
+            colorscheme->fFontFlags = fontFlags;
+            ptbmod->UpdateColorScheme();
+        }
+    }
+}
+
+uint8_t pyGUIControlTextBox::GetFontFlags()
+{
+    if (fGCkey)
+    {
+        // get the pointer to the modifier
+        pfGUITextBoxMod* ptbmod = pfGUITextBoxMod::ConvertNoRef(fGCkey->ObjectIsLoaded());
+        if (ptbmod)
+        {
+            pfGUIColorScheme* colorscheme = ptbmod->GetColorScheme();
+            return colorscheme->fFontFlags;
+        }
+    }
+    return 0;
+}

Sources/Plasma/FeatureLib/pfPython/pyGUIControlTextBox.h
@@ -86,8 +86,10 @@ public:
     virtual void    SetForeColor( pyColor& color );
     virtual void    SetBackColor( pyColor& color );
     virtual void    SetJustify( uint8_t justify );
+    virtual void    SetFontFlags(uint8_t fontflags);
     virtual uint8_t   GetJustify();
     virtual PyObject* GetForeColor(); // returns pyColor
+    virtual uint8_t GetFontFlags();
 };
 
 #endif // _pyGUIControlTextBox_h_