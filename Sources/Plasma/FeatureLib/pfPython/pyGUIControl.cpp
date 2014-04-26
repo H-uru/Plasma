openuruunofficial/CWE-ou

Move methods to pyGUIControl to pyGUIControlTextBox.
--HG--
branch : textrendering

MOULOpenSourceClientPlugin/Plasma20/Sources/Plasma/FeatureLib/pfPython/pyGUIControl.cpp
@@ -444,3 +444,33 @@ void pyGUIControl::SetFontSize(UInt32 fontsize)
 		}
 	}
 }
+
+void pyGUIControl::SetFontFlags(UInt8 fontFlags)
+{
+	if (fGCkey)
+	{
+		// get the pointer to the modifier
+		pfGUIControlMod* pdmod = pfGUIControlMod::ConvertNoRef(fGCkey->ObjectIsLoaded());
+		if (pdmod)
+		{
+			pfGUIColorScheme* colorscheme = pdmod->GetColorScheme();
+			colorscheme->fFontFlags = fontFlags;
+		}
+	}
+}
+
+UInt8 pyGUIControl::GetFontFlags()
+{
+	if (fGCkey)
+	{
+		// get the pointer to the modifier
+		pfGUIControlMod* pdmod = pfGUIControlMod::ConvertNoRef(fGCkey->ObjectIsLoaded());
+		if (pdmod)
+		{
+			pfGUIColorScheme* colorscheme = pdmod->GetColorScheme();
+			return colorscheme->fFontFlags;
+		}
+	}
+	return 0;
+}
+

H-uru/Plasma

Sources/Plasma/FeatureLib/pfPython/pyGUIControl.cpp
@@ -445,3 +445,33 @@ void pyGUIControl::SetFontSize(uint32_t fontsize)
         }
     }
 }
+
+void pyGUIControl::SetFontFlags(uint8_t fontFlags)
+{
+    if (fGCkey)
+    {
+        // get the pointer to the modifier
+        pfGUIControlMod* pdmod = pfGUIControlMod::ConvertNoRef(fGCkey->ObjectIsLoaded());
+        if (pdmod)
+        {
+            pfGUIColorScheme* colorscheme = pdmod->GetColorScheme();
+            colorscheme->fFontFlags = fontFlags;
+        }
+    }
+}
+
+uint8_t pyGUIControl::GetFontFlags()
+{
+    if (fGCkey)
+    {
+        // get the pointer to the modifier
+        pfGUIControlMod* pdmod = pfGUIControlMod::ConvertNoRef(fGCkey->ObjectIsLoaded());
+        if (pdmod)
+        {
+            pfGUIColorScheme* colorscheme = pdmod->GetColorScheme();
+            return colorscheme->fFontFlags;
+        }
+    }
+    return 0;
+}
+