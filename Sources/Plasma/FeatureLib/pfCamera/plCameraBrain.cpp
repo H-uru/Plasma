H-uru/Plasma

commit 9c49b57580c96b8b7bb208853d3f45c7fbd8e368

Fix argument ordering on fixed camera targets

Sources/Plasma/FeatureLib/pfCamera/plCameraBrain.cpp
@@ -1567,7 +1567,7 @@ void plCameraBrain1_Fixed::Update(bool forced)
                 fTargetMatrix = fCamera->GetTarget()->GetCoordinateInterface()->GetLocalToWorld();
                 hsVector3 view;
                 hsVector3 up;
-                fTargetMatrix.GetAxis(0, &view, &up);
+                fTargetMatrix.GetAxis(0, &up, &view);
                 fGoal = fTargetMatrix.GetTranslate();
                 if (fTargetPoint)
                     fPOAGoal = fTargetPoint->GetBrain()->GetGoal();

commit 1835e6b5c616d494ada8c95a365a977a0d3ad5fa

Fix weird camera angles caused by 9c49b57
The reason for the previous commit was that coverity indicated view was
being passed to up and vice versa. I don't know what they were intending,
but the calculation was correct, just the argument naming was wrong. This
has all been corrected now.

Sources/Plasma/FeatureLib/pfCamera/plCameraBrain.cpp
@@ -1565,14 +1565,13 @@ void plCameraBrain1_Fixed::Update(bool forced)
             if (fCamera->GetTarget())
             {
                 fTargetMatrix = fCamera->GetTarget()->GetCoordinateInterface()->GetLocalToWorld();
-                hsVector3 view;
                 hsVector3 up;
-                fTargetMatrix.GetAxis(0, &up, &view);
+                fTargetMatrix.GetAxis(nullptr, &up, nullptr);
                 fGoal = fTargetMatrix.GetTranslate();
                 if (fTargetPoint)
                     fPOAGoal = fTargetPoint->GetBrain()->GetGoal();
                 else
-                    fPOAGoal = fGoal - (view * 10);
+                    fPOAGoal = fGoal - (up * 10);
             }
         }
         if (fFlags.IsBitSet(kRailComponent) && fRail)