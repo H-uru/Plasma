ac857865d4656809bbb1e782dd753eff2e26429d
Darryl Pogue <darryl@dpogue.ca>
2016-01-08 00:27:44 -0800

    Refactor the texture loading

--- Sources/Plasma/FeatureLib/pfMetalPipeline/plMetalPipeline.cpp
+++ Sources/Plasma/FeatureLib/pfMetalPipeline/plMetalPipeline.cpp
@@ -4196,17 +4196,6 @@ void plMetalPipeline::IBlendVertBuffer(plSpan* span, hsMatrix44* matrixPalette, 
 
 // Resource checking
 
-// CheckTextureRef //////////////////////////////////////////////////////
-// Make sure the given layer's texture has background D3D resources allocated.
-void plMetalPipeline::CheckTextureRef(plLayerInterface* layer)
-{
-    plBitmap* bitmap = layer->GetTexture();
-
-    if (bitmap) {
-        CheckTextureRef(bitmap);
-    }
-}
-
 void plMetalPipeline::CheckTextureRef(plBitmap* bitmap)
 {
     plMetalTextureRef* tRef = static_cast<plMetalTextureRef*>(bitmap->GetDeviceRef());
@@ -4228,7 +4217,7 @@ hsGDeviceRef* plMetalPipeline::MakeTextureRef(plBitmap* bitmap)
     if (!tRef) {
         tRef = new plMetalTextureRef();
 
-        fDevice.SetupTextureRef(bitmap, tRef);
+        fDevice.SetupTextureRef(nullptr, bitmap, tRef);
     }
 
     if (!tRef->IsLinked()) {
@@ -4249,13 +4238,13 @@ void plMetalPipeline::IReloadTexture(plBitmap* bitmap, plMetalTextureRef* ref)
 {
     plMipmap* mip = plMipmap::ConvertNoRef(bitmap);
     if (mip) {
-        fDevice.MakeTextureRef(ref, mip);
+        fDevice.MakeTextureRef(ref, nullptr, mip);
         return;
     }
 
     plCubicEnvironmap* cubic = plCubicEnvironmap::ConvertNoRef(bitmap);
     if (cubic) {
-        fDevice.MakeCubicTextureRef(ref, cubic);
+        fDevice.MakeCubicTextureRef(ref, nullptr, cubic);
         return;
     }
 }
