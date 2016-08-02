d83a739f3b1e281a83d68306b3a0f37e4ed4f580
Darryl Pogue <darryl@dpogue.ca>
2016-08-01 19:08:32 -0700

    Add initial pass of GL runtime lighting

--- Sources/Plasma/FeatureLib/pfGLPipeline/plShaderNode.cpp
+++ Sources/Plasma/FeatureLib/pfGLPipeline/plShaderNode.cpp
@@ -181,7 +181,22 @@ ST::string plShaderContext::RenderNode(std::shared_ptr<plShaderNode> node, std::
         {
             std::shared_ptr<plConditionNode> cond = static_pointer_cast<plConditionNode>(node);
 
-            return ST::format("if ({}) {{ {}; }", this->RenderNode(cond->condition, fn), this->RenderNode(cond->body, fn));
+            ST::string_stream out;
+            out << ST::format("if ({}) {{", this->RenderNode(cond->condition, fn));
+
+            if (cond->nodes.size() == 1) {
+                out << ST::format(" {}; }", this->RenderNode(cond->nodes[0], fn));
+            } else {
+                out << "\n";
+
+                for (size_t i = 0; i < cond->nodes.size(); i++) {
+                    out << "\t" << this->RenderNode(cond->nodes[i], fn) << ";\n";
+                }
+
+                out << "}";
+            }
+
+            return out.to_string();
         }
         break;
 
@@ -208,14 +223,39 @@ ST::string plShaderContext::Render()
     if (this->type == kFragment)
         out << "precision mediump float;\n";
 
-    for (std::shared_ptr<plAttributeNode> node : this->attributes)
-        out << ST::format("attribute {} {};\n", node->type, node->name);
+    for (std::shared_ptr<plShaderStruct> st : this->structs) {
+        out << ST::format("struct {} {{\n", st->name);
 
-    for (std::shared_ptr<plUniformNode> node : this->uniforms)
-        out << ST::format("uniform {} {};\n", node->type, node->name);
+        for (std::shared_ptr<plVariableNode> var : st->fields) {
+            if (var->count > 1)
+                out << "\t" << ST::format("{} {}[{}];\n", var->type, var->name, var->count);
+            else
+                out << "\t" << ST::format("{} {};\n", var->type, var->name);
+        }
 
-    for (std::shared_ptr<plVaryingNode> node : this->varyings)
-        out << ST::format("varying {} {};\n", node->type, node->name);
+        out << "};\n";
+    }
+
+    for (std::shared_ptr<plAttributeNode> node : this->attributes) {
+        if (node->count > 1)
+            out << ST::format("attribute {} {}[{}];\n", node->type, node->name, node->count);
+        else
+            out << ST::format("attribute {} {};\n", node->type, node->name);
+    }
+
+    for (std::shared_ptr<plUniformNode> node : this->uniforms) {
+        if (node->count > 1)
+            out << ST::format("uniform {} {}[{}];\n", node->type, node->name, node->count);
+        else
+            out << ST::format("uniform {} {};\n", node->type, node->name);
+    }
+
+    for (std::shared_ptr<plVaryingNode> node : this->varyings) {
+        if (node->count > 1)
+            out << ST::format("varying {} {}[{}];\n", node->type, node->name, node->count);
+        else
+            out << ST::format("varying {} {};\n", node->type, node->name);
+    }
 
 
     for (std::shared_ptr<plShaderFunction> fn : this->funcs) {
@@ -233,8 +273,12 @@ ST::string plShaderContext::Render()
         out << ") {\n";
 
 
-        for (std::shared_ptr<plTempVariableNode> node : fn->temps)
-            out << "\t" << ST::format("{} {};\n", node->type, node->name);
+        for (std::shared_ptr<plTempVariableNode> node : fn->temps) {
+            if (node->count > 1)
+                out << "\t" << ST::format("{} {}[{}];\n", node->type, node->name, node->count);
+            else
+                out << "\t" << ST::format("{} {};\n", node->type, node->name);
+        }
 
 
         if (fn->temps.size())
