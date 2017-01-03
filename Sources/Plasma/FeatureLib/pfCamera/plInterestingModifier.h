dd410fa5a9de19af775a9887c654c6e94b854711
Joseph Davies <deledrius@gmail.com>
2017-01-03 01:41:09 -0800

    Fix Interesting typos.

--- Sources/Plasma/FeatureLib/pfCamera/plInterestingModifier.h
+++ Sources/Plasma/FeatureLib/pfCamera/plInterestingModifier.h
@@ -81,8 +81,8 @@ public:
     float GetInterestWeight() { return fInterestWeight; }
     float GetInterestRadius() { return fInterestRadius; }
 
-    void SetInterestWeight(float _InterestRadius) { fInterestWeight =_InterestRadius; }
-    void SetInterestRadius(float _InterestWeight) { fInterestRadius =_InterestWeight; }
+    void SetInterestWeight(float _InterestWeight) { fInterestWeight = _InterestWeight; }
+    void SetInterestRadius(float _InterestRadius) { fInterestRadius = _InterestRadius; }
     
     virtual void AddTarget(plSceneObject* so);
     
