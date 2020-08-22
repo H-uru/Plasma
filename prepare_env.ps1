563bb3ed0bc1ead7caf38cc501f75e9a2b6d2796
Adam Johnson <AdamJohnso@gmail.com>
2020-08-21 21:01:39 -0400

    Copy the Python3 stdlib as expected by plPythonPack.
    A common issue, reported in:
    - https://forum.guildofwriters.org/viewtopic.php?f=10&t=7156
    - https://forum.guildofwriters.org/viewtopic.php?p=74562#p74562

--- prepare_env.ps1
+++ prepare_env.ps1
@@ -40,6 +40,10 @@ if (Get-ChildItem Env:PATH | Where-Object {$_.Value -match "CMake"}) {
           -DPython3_LIBRARY=$path/devlibs/lib/python38.lib -DPython3_INCLUDE_DIR=$path/devlibs/include `
           -DPLASMA_BUILD_TOOLS=OFF -DPLASMA_BUILD_RESOURCE_DAT=OFF `
           -A Win32 -G "Visual Studio 15 2017" $source_path
+    if ($lastExitCode -Ne 0) { throw "Failed to configure build system!" }
+    Write-Host "Copying Python 3 dist-packages..."
+    cmake --build . --target ScriptsSystem
+    if ($lastExitCode -Ne 0) { throw "Failed to copy Python dist-packages!" }
 } else {
     Write-Host "CMake not found in PATH."
     Write-Host "Please run the CMake installer and select the option to add CMake to your system PATH."
