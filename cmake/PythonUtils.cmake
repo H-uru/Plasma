c709d4f7f8e9e5cf930fc8c713e49122901d7c74
Adam Johnson <AdamJohnso@gmail.com>
2020-12-21 16:01:25 -0500

    Merge `python_install_modules()` into `python_test_modules()`.

--- cmake/PythonUtils.cmake
+++ cmake/PythonUtils.cmake
@@ -3,10 +3,45 @@
 option(ENABLE_PIP_INSTALL "Allow automatic installation of Python dependency modules." OFF)
 
 function(python_test_modules)
-    cmake_parse_arguments(_ptm "REQUIRED" "" "MODULES" ${ARGN})
+    cmake_parse_arguments(_ptm "DONT_INSTALL;REQUIRED" "REQUIREMENTS_FILE" "MODULES" ${ARGN})
 
     if(NOT _ptm_MODULES)
         message(FATAL_ERROR "python_test_modules() must be given a list of MODULES")
+    endif()
+
+    # Optional: attempt to install modules from pip before checking their existence.
+    if(ENABLE_PIP_INSTALL AND NOT _ptm_DONT_INSTALL)
+        if(NOT _PLASMA_ENSUREPIP_DONE)
+            message(STATUS "Bootstrapping pip...")
+            execute_process(
+                COMMAND "${Python3_EXECUTABLE}" -m ensurepip
+                RESULT_VARIABLE RETURNCODE
+                OUTPUT_QUIET ERROR_QUIET
+            )
+            if(RETURNCODE)
+                message(WARNING "Unable to bootstrap pip")
+            endif()
+            set(_PLASMA_ENSUREPIP_DONE ON CACHE INTERNAL "" FORCE)
+        endif()
+
+        if(_ptm_REQUIREMENTS_FILE)
+            message(STATUS "Installing Python modules from: ${_ptm_REQUIREMENTS_FILE}")
+            execute_process(
+                COMMAND "${Python3_EXECUTABLE}" -m pip install -r "${_ptm_REQUIREMENTS_FILE}"
+                RESULT_VARIABLE RETURNCODE
+                OUTPUT_QUIET ERROR_QUIET
+            )
+        else()
+            message(STATUS "Installing Python modules: ${_ptm_MODULES}")
+            execute_process(
+                COMMAND "${Python3_EXECUTABLE}" -m pip install ${_ptm_MODULES}
+                OUTPUT_QUIET ERROR_QUIET
+            )
+        endif()
+
+        if(RETURNCODE)
+            message(WARNING "Failed to install modules")
+        endif()
     endif()
 
     foreach(_module_NAME IN LISTS _ptm_MODULES)
@@ -29,58 +64,4 @@ function(python_test_modules)
             message(FATAL_ERROR "Could NOT find Python3 Module: ${_module_NAME}")
         endif()
     endforeach()
-endfunction()
-
-function(python_install_modules)
-    cmake_parse_arguments(_pim "" "REQUIREMENTS_FILE" "MODULES" ${ARGN})
-
-    if(NOT _pim_REQUIREMENTS_FILE AND NOT _pim_MODULES)
-        message(FATAL_ERROR "python_install_modules() requires either a REQUIREMENTS_FILE or a list of MODULES")
-    endif()
-    if(NOT Python3_EXECUTABLE)
-        message(WARNING "Python3 is not available, not installing requested modules.")
-    endif()
-
-    # Escape hatch for folks who don't want us mucking around with their system Python.
-    if(NOT ENABLE_PIP_INSTALL)
-        return()
-    endif()
-
-    if(NOT _PLASMA_ENSUREPIP_DONE)
-        message(STATUS "Bootstrapping pip...")
-        execute_process(
-            COMMAND "${Python3_EXECUTABLE}" -m ensurepip
-            RESULT_VARIABLE RETURNCODE
-            OUTPUT_QUIET ERROR_QUIET
-        )
-        if(RETURNCODE)
-            message(WARNING "Unable to bootstrap pip")
-            return()
-        endif()
-        set(_PLASMA_ENSUREPIP_DONE ON CACHE INTERNAL "" FORCE)
-    endif()
-
-    if(_pim_REQUIREMENTS_FILE)
-        message(STATUS "Installing Python modules from: ${_pim_REQUIREMENTS_FILE}")
-        execute_process(
-            COMMAND "${Python3_EXECUTABLE}" -m pip install -r "${_pim_REQUIREMENTS_FILE}"
-            RESULT_VARIABLE RETURNCODE
-            OUTPUT_QUIET ERROR_QUIET
-        )
-        if(RETURNCODE)
-            message(WARNING "Failed to install modules")
-            unset(RETURNCODE)
-        endif()
-    endif()
-
-    if(_pim_MODULES)
-        message(STATUS "Installing Python modules: ${_pim_MODULES}")
-        execute_process(
-            COMMAND "${Python3_EXECUTABLE}" -m pip install ${_pim_MODULES}
-            OUTPUT_QUIET ERROR_QUIET
-        )
-        if(RETURNCODE)
-            message(WARNING "Failed to install modules")
-        endif()
-    endif()
 endfunction()
