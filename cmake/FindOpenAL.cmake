47dbe122fb92d2beb79a0258ea98795aa5ec9718
Adam Johnson <AdamJohnso@gmail.com>
2020-12-22 17:56:21 -0500

    Don't override default cmake behavior.

--- cmake/FindOpenAL.cmake
+++ cmake/FindOpenAL.cmake
@@ -1,54 +1,20 @@
-# Derived from the builtin FindOpenAL.cmake, but with less idiocy.
+# First, try to load OpenAL-Soft from its Cmake config.
+plasma_forward_find_package(OpenAL CONFIG)
 
-include(FindPackageHandleStandardArgs)
-
-find_path(
-    OpenAL_INCLUDE_DIR al.h
-    HINTS ENV OpenALDIR
-    PATHS
-        ~/Library/Frameworks
-        /Library/Frameworks
-        /opt
-        [HKEY_LOCAL_MACHINE\\SOFTWARE\\Creative\ Labs\\OpenAL\ 1.1\ Software\ Development\ Kit\\1.00.0000;InstallDir]
-        PATH_SUFFIXES include/AL include/OpenAL include AL OpenAL
-)
-
-if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-    set(_OpenAL_ARCH_DIR libs/Win64)
-else()
-    set(_OpenAL_ARCH_DIR libs/Win32)
-endif()
-
-find_library(
-    OpenAL_LIBRARY
-    NAMES OpenAL al openal OpenAL32
-    HINTS ENV OpenALDIR
-    PATHS
-        ~/Library/Frameworks
-        /Library/Frameworks
-        /opt
-        [HKEY_LOCAL_MACHINE\\SOFTWARE\\Creative\ Labs\\OpenAL\ 1.1\ Software\ Development\ Kit\\1.00.0000;InstallDir]
-        PATH_SUFFIXES libx32 lib64 lib libs64 libs ${_OpenAL_ARCH_DIR}
-)
-
-unset(_OpenAL_ARCH_DIR)
-
-find_package_handle_standard_args(
-    OpenAL
-    REQUIRED_VARS OpenAL_LIBRARY OpenAL_INCLUDE_DIR
-    VERSION_VAR OpenAL_VERSION_STRING
-)
-
-mark_as_advanced(OpenAL_LIBRARY OpenAL_INCLUDE_DIR)
-
+# If that didn't work, fall back to the CMake find module
 if(NOT TARGET OpenAL::OpenAL)
-    add_library(OpenAL::OpenAL UNKNOWN IMPORTED)
-    set_target_properties(
-        OpenAL::OpenAL PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES ${OpenAL_INCLUDE_DIR}
-        IMPORTED_LOCATION ${OpenAL_LIBRARY}
-    )
-endif()
+    plasma_forward_find_package(OpenAL MODULE ONLY_CMAKE_FIND_ROOT_PATH)
 
-set(OpenAL_INCLUDE_DIRS ${OpenAL_INCLUDE_DIR})
-set(OpenAL_LIBRARIES ${OpenAL_LIBRARY})
+    if(OPENAL_FOUND)
+        if(NOT TARGET OpenAL::OpenAL)
+            add_library(OpenAL::OpenAL UNKNOWN IMPORTED)
+            set_target_properties(
+                OpenAL::OpenAL PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES ${OPENAL_INCLUDE_DIR}
+                IMPORTED_LOCATION ${OPENAL_LIBRARY}
+            )
+        endif()
+    elseif(OpenAL_FIND_REQUIRED)
+        message(FATAL_ERROR "Could NOT find OpenAL")
+    endif()
+endif()
