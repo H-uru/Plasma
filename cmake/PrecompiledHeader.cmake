17397a02cfcfc9bb357c7d78307945821a389474
Adam Johnson <AdamJohnso@gmail.com>
2021-01-10 18:20:55 -0500

    Use CMake 3.16 precompiled header support.
    Visual Studio's Ninja support doesn't like what we were doing before.
    This is better in all ways and also supports gcc.

--- cmake/PrecompiledHeader.cmake
+++ cmake/PrecompiledHeader.cmake
@@ -1,27 +1,13 @@
-if(MSVC)
-    set(PCH_SUPPORTED TRUE)
-else()
-    set(PCH_SUPPORTED FALSE)
-endif()
+cmake_dependent_option(
+    PLASMA_USE_PCH
+    "Enable precompiled headers?"
+    ON
+    [[${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.16"]]
+    OFF
+)
 
-if(PCH_SUPPORTED)
-    option(PLASMA_USE_PCH "Enable precompiled headers?" ON)
-endif(PCH_SUPPORTED)
-
-function(use_precompiled_header PrecompiledHeader PrecompiledSource Headers Sources)
+function(use_precompiled_header TARGET)
     if(PLASMA_USE_PCH)
-        if(MSVC)
-            get_filename_component(PrecompiledBasename ${PrecompiledHeader} NAME_WE)
-            set(PrecompiledBinary ${PrecompiledBasename}.pch)
-
-            add_definitions(/Yu"${PrecompiledHeader}")
-            add_definitions(/FI"${PrecompiledHeader}")
-            set_source_files_properties(${PrecompiledSource} PROPERTIES COMPILE_FLAGS "/Yc\"${PrecompiledHeader}\"")
-        endif(MSVC)
-
-        # Add the Pch.[h|cpp] to the appropriate sets
-        # OT: This has to be the oddest thing I've ever written =/
-        set(${Headers} ${${Headers}} ${PrecompiledHeader} PARENT_SCOPE)
-        set(${Sources} ${${Sources}} ${PrecompiledSource} PARENT_SCOPE)
+        target_precompile_headers(${TARGET} PRIVATE ${ARGN})
     endif(PLASMA_USE_PCH)
 endfunction(use_precompiled_header)
