f472d9abb5091391fddf8c29472fc6a5c39bd27f
Adam Johnson <AdamJohnso@gmail.com>
2021-01-10 11:24:10 -0500

    Improve support for Visual Studio CMake.

--- cmake/VcpkgToolchain.cmake
+++ cmake/VcpkgToolchain.cmake
@@ -14,26 +14,21 @@ set(PLASMA_VCPKG_NUGET_OWNER "" CACHE INTERNAL "")
 set(PLASMA_VCPKG_NUGET_TOKEN "" CACHE INTERNAL "")
 set(PLASMA_VCPKG_NUGET_RW FALSE CACHE INTERNAL "")
 
-# Expected toolchain value
-set(_PLASMA_VCPKG_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake")
-
 # Welcome to vcpkg-land. Population: Hoikas.
 # The goal here is that if cmake is being invoked with no arguments, we want to force usage of
 # our vcpkg submodule on Windows hosts ONLY. Otherwise, it's an opt-in type dealio. Further,
-# if the consumer is already using a toolchain, we don't want to sweep the rug out from under them.
+# if the consumer is already using a toolchain, we would ideally not want to sweep the rug out
+# from under them. The good news is that vcpkg will detect any old non-manifest build directories
+# and refuse to continue on that, so great... BUT WAIT! Stoopid Visual Studio's CMake support will
+# pass in an arbitrary vcpkg toolchain if one is not specified in CMakeSettings.json. We don't want
+# that either. Ugh, screw me.
 string(COMPARE EQUAL "${CMAKE_HOST_SYSTEM_NAME}" "Windows" _HOST_IS_WINDOWS)
-cmake_dependent_option(
-    USE_VCPKG
-    "Use the vcpkg submodule for dependency management."
-    ${_HOST_IS_WINDOWS}
-    [[NOT DEFINED CMAKE_TOOLCHAIN_FILE OR "${CMAKE_TOOLCHAIN_FILE}" STREQUAL "${_PLASMA_VCPKG_TOOLCHAIN_FILE}"]]
-    FALSE
-)
+option(USE_VCPKG "Use the vcpkg submodule for dependency management." ${_HOST_IS_WINDOWS})
 if(NOT USE_VCPKG)
     return()
 endif()
 
-set(CMAKE_TOOLCHAIN_FILE "${_PLASMA_VCPKG_TOOLCHAIN_FILE}" CACHE STRING "")
+set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE STRING "" FORCE)
 
 function(_plasma_vcpkg_generate_nuget_config)
     cmake_parse_arguments(_pvgnc "" "OUT_FILE;NUGET_NAME;NUGET_SOURCE;NUGET_OWNER;NUGET_TOKEN" "" ${ARGN})
@@ -66,21 +61,29 @@ function(_plasma_vcpkg_setup_binarycache)
     endif()
 endfunction()
 
-if(NOT ENV{VCPKG_BINARY_SOURCES})
-    set(ENV{VCPKG_BINARY_SOURCES} "clear;default,read")
-endif()
 _plasma_vcpkg_setup_binarycache(NAME mainline PREFIX _NUGET)
 _plasma_vcpkg_setup_binarycache(NAME fork PREFIX PLASMA_VCPKG_NUGET)
 
 # Note that CMAKE_SIZEOF_VOID_P is currently undefined.
-if(_HOST_IS_WINDOWS)
-    set(VCPKG_OVERLAY_TRIPLETS "${CMAKE_SOURCE_DIR}/Scripts/Triplets")
-    if("${CMAKE_GENERATOR_PLATFORM}" MATCHES "[Ww][Ii][Nn]32")
+list(APPEND VCPKG_OVERLAY_TRIPLETS "${CMAKE_SOURCE_DIR}/Scripts/Triplets")
+if(_HOST_IS_WINDOWS AND NOT DEFINED VCPKG_TARGET_TRIPLET)
+    if(CMAKE_GENERATOR_PLATFORM MATCHES "[Ww][Ii][Nn]32")
         set(VCPKG_TARGET_TRIPLET "x86-windows-plasma" CACHE STRING "")
-    elseif("${CMAKE_GENERATOR_PLATFORM}" MATCHES "[Xx]64")
+    elseif(CMAKE_GENERATOR_PLATFORM MATCHES "[Xx]64")
         set(VCPKG_TARGET_TRIPLET "x64-windows-plasma" CACHE STRING "")
+    elseif(NOT CMAKE_GENERATOR_PLATFORM)
+        # This is probably the Ninja generator. Unfortunately, we can't try_compile() anything
+        # right now, so we'll just steal part of the vcpkg toolchain to maybe guess the triplet.
+        find_program(_VCPKG_CL cl)
+        if(_VCPKG_CL MATCHES "amd64/cl.exe$" OR _VCPKG_CL MATCHES "x64/cl.exe$")
+            set(VCPKG_TARGET_TRIPLET "x64-windows-plasma" CACHE STRING "")
+        elseif(_VCPKG_CL MATCHES "bin/cl.exe$" OR _VCPKG_CL MATCHES "x86/cl.exe$")
+            set(VCPKG_TARGET_TRIPLET "x86-windows-plasma" CACHE STRING "")
+        else()
+            message(FATAL_ERROR "Unknown compiler: '${_VCPKG_CL}' - set VCPKG_TARGET_TRIPLET manually.")
+        endif()
     else()
-        message(FATAL_ERROR "Unsupported platform: ${CMAKE_GENERATOR_PLATFORM}")
+        message(FATAL_ERROR "Unknown platform: '${CMAKE_GENERATOR_PLATFORM}' - set VCPKG_TARGET_TRIPLET manually.")
     endif()
 endif()
 
