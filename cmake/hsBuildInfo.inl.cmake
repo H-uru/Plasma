a472397189e7191386f5df1ea5477b856d1ae5d4
Adam Johnson <AdamJohnso@gmail.com>
2021-02-11 21:58:25 -0500

    Move plProduct info into a generated file.
    This has a few benefits:
    - Changing an aspect of the product rebuilds only plProduct.cpp instead
      of the entire CoreLib project.
    - We can now include more information, like the git tag/ref and build
      time. This is useful because the build ID is usually always 918 and is
      more to-do with the client/server handshake than an actual build
      identifier.
    
    If working on a large changeset, you can bypass overhead from this by
    setting `-DPRODUCT_EMBED_BUILD_INFO=OFF`.

---
+++ cmake/hsBuildInfo.inl.cmake
@@ -0,0 +1,20 @@
+// Automatically generated for diff:
+// @CURRENT_DIFF_HASH@
+
+namespace plProduct
+{
+    constexpr uint32_t PRODUCT_BRANCH_ID = @PRODUCT_BRANCH_ID@;
+    constexpr uint32_t PRODUCT_BUILD_ID = @PRODUCT_BUILD_ID@;
+    constexpr uint32_t PRODUCT_BUILD_TYPE = @PRODUCT_BUILD_TYPE@;
+
+    static const ST::string PRODUCT_CORE_NAME = ST_LITERAL("@PRODUCT_CORE_NAME@");
+    static const ST::string PRODUCT_SHORT_NAME = ST_LITERAL("@PRODUCT_SHORT_NAME@");
+    static const ST::string PRODUCT_LONG_NAME = ST_LITERAL("@PRODUCT_LONG_NAME@");
+    constexpr std::string_view PRODUCT_UUID = "@PRODUCT_UUID@";
+
+    static const ST::string GIT_REV = ST_LITERAL("@GIT_REV@");
+    static const ST::string GIT_TAG = ST_LITERAL("@GIT_TAG@");
+
+    static const ST::string VOLATILE_BUILD_DATE = ST_LITERAL("@BUILD_DATE@");
+    static const ST::string VOLATILE_BUILD_TIME = ST_LITERAL("@BUILD_TIME@");
+};
