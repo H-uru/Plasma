824fbe3d1eff0433560c20fc00bb4f83a694d9c1
Adam Johnson <AdamJohnso@gmail.com>
2021-02-13 00:18:10 -0500

    Target-ize libuuid.

--- cmake/FindUuid.cmake
+++ cmake/FindUuid.cmake
@@ -11,5 +11,11 @@ find_library(Uuid_LIBRARY NAMES uuid
 
 find_package_handle_standard_args(Uuid REQUIRED_VARS Uuid_INCLUDE_DIR Uuid_LIBRARY)
 
-set(Uuid_INCLUDE_DIRS ${Uuid_INCLUDE_DIRS})
-set(Uuid_LIBRARIES ${Uuid_LIBRARY})
+if(Uuid_FOUND AND NOT TARGET Uuid::Uuid)
+    add_library(Uuid::Uuid UNKNOWN IMPORTED)
+    set_target_properties(
+        Uuid::Uuid PROPERTIES
+        IMPORTED_LOCATION ${Uuid_LIBRARY}
+        INTERFACE_INCLUDE_DIRECTORIES ${Uuid_INCLUDE_DIR}
+    )
+endif()
