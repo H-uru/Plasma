90dc06f32fe2c27ea4fd7c6c1dc7c1294213eb37
Michael Hansen <zrax0111@gmail.com>
2021-04-02 11:51:36 -0700

    Add optional build system support for Qt6

--- cmake/QtDeployment.cmake
+++ cmake/QtDeployment.cmake
@@ -1,29 +1,49 @@
-find_package(Qt5 COMPONENTS Core Widgets)
+find_package(Qt6 QUIET COMPONENTS Core Widgets)
+set(Qt_FOUND ${Qt6_FOUND})
+set(_qt_qmake_target Qt6::qmake)
+
+if(NOT Qt6_FOUND)
+    find_package(Qt5 COMPONENTS Core Widgets)
+    set(Qt_FOUND ${Qt5_FOUND})
+    set(_qt_qmake_target Qt5::qmake)
+
+    if(Qt5_FOUND AND NOT TARGET Qt::Core)
+        # The version-generic targets were only added in Qt 5.15
+        add_library(Qt::Core INTERFACE IMPORTED)
+        set_target_properties(Qt::Core PROPERTIES
+            INTERFACE_LINK_LIBRARIES "Qt5::Core"
+        )
+        add_library(Qt::Widgets INTERFACE IMPORTED)
+        set_target_properties(Qt::Widgets PROPERTIES
+            INTERFACE_LINK_LIBRARIES "Qt5::Widgets"
+        )
+    endif()
+endif()
 
 # Based on https://stackoverflow.com/a/41199492 for easily deploying Qt DLLs on Windows.
-if(WIN32 AND TARGET Qt5::qmake AND NOT TARGET Qt5::windeployqt)
-    get_target_property(_qt5_qmake_location Qt5::qmake IMPORTED_LOCATION)
+if(WIN32 AND TARGET ${_qt_qmake_target} AND NOT TARGET Qt::windeployqt)
+    get_target_property(_qt_qmake_location ${_qt_qmake_target} IMPORTED_LOCATION)
 
     execute_process(
-        COMMAND "${_qt5_qmake_location}" -query QT_INSTALL_PREFIX
+        COMMAND "${_qt_qmake_location}" -query QT_INSTALL_PREFIX
         RESULT_VARIABLE return_code
-        OUTPUT_VARIABLE qt5_install_prefix
+        OUTPUT_VARIABLE qt_install_prefix
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
-    if(EXISTS "${qt5_install_prefix}/bin/windeployqt.exe")
-        add_executable(Qt5::windeployqt IMPORTED)
+    if(EXISTS "${qt_install_prefix}/bin/windeployqt.exe")
+        add_executable(Qt::windeployqt IMPORTED)
 
         set_target_properties(
-            Qt5::windeployqt PROPERTIES
-            IMPORTED_LOCATION "${qt5_install_prefix}/bin/windeployqt.exe"
+            Qt::windeployqt PROPERTIES
+            IMPORTED_LOCATION "${qt_install_prefix}/bin/windeployqt.exe"
         )
     endif()
 endif()
 
 # Allow opting into all this crazy Qt copying mess.
-cmake_dependent_option(PLASMA_APPLOCAL_QT "Deploy Qt libraries into tool binary directories" OFF "TARGET Qt5::windeployqt" OFF)
-cmake_dependent_option(PLASMA_INSTALL_QT "Deploy Qt libraries when installing Plasma" ON "TARGET Qt5::windeployqt" OFF)
+cmake_dependent_option(PLASMA_APPLOCAL_QT "Deploy Qt libraries into tool binary directories" OFF "TARGET Qt::windeployqt" OFF)
+cmake_dependent_option(PLASMA_INSTALL_QT "Deploy Qt libraries when installing Plasma" ON "TARGET Qt::windeployqt" OFF)
 
 function(plasma_deploy_qt)
     # Need to deploy Qt to the binary directory for each tool so it can be debugged more easily
@@ -34,8 +54,8 @@ function(plasma_deploy_qt)
         if(PLASMA_APPLOCAL_QT)
             add_custom_target(
                 ${i}_deployqt ALL
-                COMMAND Qt5::windeployqt --no-translations --no-angle --no-opengl-sw --dir "$<TARGET_FILE_DIR:${i}>" "$<TARGET_FILE:${i}>"
-                WORKING_DIRECTORY "$<TARGET_FILE_DIR:Qt5::windeployqt>"
+                COMMAND Qt::windeployqt --no-translations --no-angle --no-opengl-sw --dir "$<TARGET_FILE_DIR:${i}>" "$<TARGET_FILE:${i}>"
+                WORKING_DIRECTORY "$<TARGET_FILE_DIR:Qt::windeployqt>"
             )
             add_dependencies(${i}_deployqt ${i})
         endif()
@@ -48,8 +68,8 @@ function(plasma_deploy_qt)
         install(
             CODE
             "execute_process(
-                COMMAND \"$<TARGET_FILE:Qt5::windeployqt>\" --no-translations --no-angle --no-opengl-sw --dir \"${CMAKE_INSTALL_PREFIX}/tools\" \"${_DEPLOY_ARG}\"
-                WORKING_DIRECTORY \"$<TARGET_FILE_DIR:Qt5::windeployqt>\"
+                COMMAND \"$<TARGET_FILE:Qt::windeployqt>\" --no-translations --no-angle --no-opengl-sw --dir \"${CMAKE_INSTALL_PREFIX}/tools\" \"${_DEPLOY_ARG}\"
+                WORKING_DIRECTORY \"$<TARGET_FILE_DIR:Qt::windeployqt>\"
                 COMMAND_ERROR_IS_FATAL ANY
             )"
         )
