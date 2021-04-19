H-uru/Plasma

commit 44f3f64e2478384e91d1230633c033daa963c284

Port plClusterGroup to std::vector

Sources/Plasma/PubUtilLib/plDrawable/plDrawableSpans.cpp
@@ -3180,17 +3180,15 @@ void plDrawableSpans::UnPackCluster(plClusterGroup* cluster)
     const uint32_t vertsPerInst = cluster->GetTemplate()->NumVerts();
     const uint32_t idxPerInst = cluster->GetTemplate()->NumIndices();
 
-    const uint32_t numClust = cluster->GetNumClusters();
+    const size_t numClust = cluster->GetNumClusters();
 
     fVisSet |= cluster->GetVisSet();
     fVisNot |= cluster->GetVisNot();
 
     fIcicles.SetCount(numClust);
     fSpans.SetCount(numClust);
-    int iSpan;
-    for( iSpan = 0; iSpan < numClust; iSpan++ )
+    for (size_t iSpan = 0; iSpan < numClust; iSpan++)
         fSpans[iSpan] = &fIcicles[iSpan];
-    iSpan = 0;
 
     uint32_t vtxFormat =
         cluster->GetTemplate()->NumUVWs()
@@ -3200,13 +3198,12 @@ void plDrawableSpans::UnPackCluster(plClusterGroup* cluster)
 
     const std::vector<plLightInfo*>& lights = cluster->GetLights();
 
-    int iStart;
-    for( iStart = 0; iStart < cluster->GetNumClusters(); )
+    for (size_t iStart = 0; iStart < cluster->GetNumClusters(); )
     {
         int numVerts = 0;
         int numIdx = 0;
-        int iEnd;
-        for( iEnd = iStart; iEnd < cluster->GetNumClusters(); iEnd++ )
+        size_t iEnd;
+        for (iEnd = iStart; iEnd < cluster->GetNumClusters(); iEnd++)
         {
             numVerts += vertsPerInst * cluster->GetCluster(iEnd)->NumInsts();
             numIdx += idxPerInst * cluster->GetCluster(iEnd)->NumInsts();
@@ -3244,8 +3241,8 @@ void plDrawableSpans::UnPackCluster(plClusterGroup* cluster)
         uint16_t* iData = fGroups[grpIdx]->GetIndexBufferData(ibufferIdx);
         uint8_t* pvData = vData;
         uint16_t* piData = iData;
-        int i;
-        for( i = iStart; i < iEnd; i++ )
+        size_t iSpan = 0;
+        for (size_t i = iStart; i < iEnd; i++)
         {
             hsBounds3Ext bnd;
             cluster->GetCluster(i)->UnPack(pvData, piData, cellOffset, bnd);

Sources/Plasma/PubUtilLib/plDrawable/plClusterGroup.cpp
@@ -75,9 +75,8 @@ plClusterGroup::plClusterGroup()
 
 plClusterGroup::~plClusterGroup()
 {
-    int i;
-    for( i = 0; i < fClusters.GetCount(); i++ )
-        delete fClusters[i];
+    for (plCluster* cluster : fClusters)
+        delete cluster;
 
     delete fTemplate;
 }
@@ -87,7 +86,7 @@ plCluster* plClusterGroup::IAddCluster()
     plCluster* cluster = new plCluster;
     // Set the cluster's group.
     cluster->SetGroup(this);
-    fClusters.Append(cluster);
+    fClusters.emplace_back(cluster);
     return cluster;
 }
 
@@ -96,36 +95,34 @@ plCluster* plClusterGroup::IGetCluster(int i) const
     return fClusters[i]; 
 }
 
-const plCluster* plClusterGroup::GetCluster(int i) const
-{ 
-    return fClusters[i]; 
+const plCluster* plClusterGroup::GetCluster(size_t i) const
+{
+    return fClusters[i];
 }
 
 void plClusterGroup::Read(hsStream* stream, hsResMgr* mgr)
 {
     hsKeyedObject::Read(stream, mgr);
 
-    int i;
-
     fTemplate = new plSpanTemplate;
     fTemplate->Read(stream);
 
     mgr->ReadKeyNotifyMe(stream, new plGenRefMsg(GetKey(), plRefMsg::kOnCreate, -1, kRefMaterial), plRefFlags::kActiveRef);
 
-    const int numClust = stream->ReadLE32();
-    fClusters.SetCount(numClust);
-    for( i = 0; i < numClust; i++ )
+    const uint32_t numClust = stream->ReadLE32();
+    fClusters.resize(numClust);
+    for (uint32_t i = 0; i < numClust; i++)
     {
         fClusters[i] = new plCluster;
         fClusters[i]->Read(stream, this);
     }
 
-    const int numRegions = stream->ReadLE32();
-    for( i = 0; i < numRegions; i++ )
+    const uint32_t numRegions = stream->ReadLE32();
+    for (uint32_t i = 0; i < numRegions; i++)
         mgr->ReadKeyNotifyMe(stream, new plGenRefMsg(GetKey(), plRefMsg::kOnCreate, -1, kRefRegion), plRefFlags::kActiveRef);
 
-    const int numLights = stream->ReadLE32();
-    for( i = 0; i < numLights; i++ )
+    const uint32_t numLights = stream->ReadLE32();
+    for (uint32_t i = 0; i < numLights; i++)
         mgr->ReadKeyNotifyMe(stream, new plGenRefMsg(GetKey(), plRefMsg::kOnCreate, -1, kRefLight), plRefFlags::kActiveRef);
 
     fLOD.Read(stream);
@@ -142,19 +139,17 @@ void plClusterGroup::Write(hsStream* stream, hsResMgr* mgr)
 {
     hsKeyedObject::Write(stream, mgr);
 
-    int i;
-
     fTemplate->Write(stream);
 
     mgr->WriteKey(stream, fMaterial);
 
-    stream->WriteLE32(fClusters.GetCount());
-    for( i = 0; i < fClusters.GetCount(); i++ )
-        fClusters[i]->Write(stream);
+    stream->WriteLE32((uint32_t)fClusters.size());
+    for (plCluster* cluster : fClusters)
+        cluster->Write(stream);
 
-    stream->WriteLE32(fRegions.GetCount());
-    for( i = 0; i < fRegions.GetCount(); i++ )
-        mgr->WriteKey(stream, fRegions[i]);
+    stream->WriteLE32((uint32_t)fRegions.size());
+    for (plVisRegion* region : fRegions)
+        mgr->WriteKey(stream, region);
 
     stream->WriteLE32((uint32_t)fLights.size());
     for (plLightInfo* light : fLights)
@@ -178,10 +173,9 @@ bool plClusterGroup::IAddVisRegion(plVisRegion* reg)
 {
     if( reg )
     {
-        int idx = fRegions.Find(reg);
-        if( idx == fRegions.kMissingIndex )
+        if (std::find(fRegions.cbegin(), fRegions.cend(), reg) == fRegions.cend())
         {
-            fRegions.Append(reg);
+            fRegions.emplace_back(reg);
             if( reg->GetProperty(plVisRegion::kIsNot) )
                 fVisNot.SetBit(reg->GetIndex());
             else
@@ -199,10 +193,10 @@ bool plClusterGroup::IRemoveVisRegion(plVisRegion* reg)
 {
     if( reg )
     {
-        int idx = fRegions.Find(reg);
-        if( fRegions.kMissingIndex != idx )
+        auto iter = std::find(fRegions.cbegin(), fRegions.cend(), reg);
+        if (iter != fRegions.cend())
         {
-            fRegions.Remove(idx);
+            fRegions.erase(iter);
             if( reg->GetProperty(plVisRegion::kIsNot) )
                 fVisNot.ClearBit(reg->GetIndex());
             else
@@ -312,12 +306,11 @@ void plClusterGroup::SetVisible(bool visible)
     }
 }
 
-uint32_t plClusterGroup::NumInst() const
+size_t plClusterGroup::NumInst() const
 {
-    uint32_t numInst = 0;
-    int i;
-    for( i = 0; i < fClusters.GetCount(); i++ )
-        numInst += fClusters[i]->NumInsts();
+    size_t numInst = 0;
+    for (plCluster* cluster : fClusters)
+        numInst += cluster->NumInsts();
 
     return numInst;
 }

Sources/Plasma/PubUtilLib/plDrawable/plClusterGroup.h
@@ -43,7 +43,8 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 #ifndef plClusterGroup_inc
 #define plClusterGroup_inc
 
-#include "hsTemplates.h"
+#include <vector>
+
 #include "hsBitVector.h"
 #include "plRenderLevel.h"
 #include "pnKeyedObject/hsKeyedObject.h"
@@ -92,7 +93,7 @@ protected:
 
     hsGMaterial*                    fMaterial;
 
-    hsTArray<plVisRegion*>          fRegions;
+    std::vector<plVisRegion*>       fRegions;
     hsBitVector                     fVisSet;
     hsBitVector                     fVisNot;
 
@@ -100,8 +101,8 @@ protected:
 
     plLODDist                       fLOD;
 
-    hsTArray<plCluster*>            fClusters;
-    uint32_t                          fUnPacked;
+    std::vector<plCluster*>         fClusters;
+    uint32_t                        fUnPacked;
 
     plKey                           fSceneNode;
     plKey                           fDrawable;
@@ -142,9 +143,9 @@ public:
 
     const plSpanTemplate* GetTemplate() const { return fTemplate; }
 
-    const plCluster* GetCluster(int i) const;
-    int         GetNumClusters() const { return fClusters.GetCount(); }
-    uint32_t      NumInst() const;
+    const plCluster* GetCluster(size_t i) const;
+    size_t GetNumClusters() const { return fClusters.size(); }
+    size_t NumInst() const;
 
     // The drawable needs us to be able to convert our data
     // into, well, drawable stuff.

commit da68e2869c91988c1f1be8d4b69b6e0d5dad19b5

Fix missing cluster span regression:
Move span iterator back out to its original scope.
Fixes a regression from 44f3f64e2478384e91d1230633c033daa963c284, which
caused cluster spans to be missing (e.g. Minkata craters)

Sources/Plasma/PubUtilLib/plDrawable/plDrawableSpans.cpp
@@ -2991,8 +2991,10 @@ void plDrawableSpans::UnPackCluster(plClusterGroup* cluster)
 
     fIcicles.resize(numClust);
     fSpans.resize(numClust);
-    for (size_t iSpan = 0; iSpan < numClust; iSpan++)
+    size_t iSpan;
+    for (iSpan = 0; iSpan < numClust; iSpan++)
         fSpans[iSpan] = &fIcicles[iSpan];
+    iSpan = 0;
 
     uint32_t vtxFormat =
         cluster->GetTemplate()->NumUVWs()
@@ -3045,7 +3047,6 @@ void plDrawableSpans::UnPackCluster(plClusterGroup* cluster)
         uint16_t* iData = fGroups[grpIdx]->GetIndexBufferData(ibufferIdx);
         uint8_t* pvData = vData;
         uint16_t* piData = iData;
-        size_t iSpan = 0;
         for (size_t i = iStart; i < iEnd; i++)
         {
             hsBounds3Ext bnd;