173af8f72d800924ceca87ff21e375396f683e21
Darryl Pogue <darryl@dpogue.ca>
2022-02-23 22:12:15 -0800

    Use manifest to set commctrl directives
    This resolves link issues with clang-cl/lld-link

--- Sources/Plasma/Apps/plClient/winmain.cpp
+++ Sources/Plasma/Apps/plClient/winmain.cpp
@@ -1275,8 +1275,3 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
     // Exit WinMain and terminate the app....
     return PARABLE_NORMAL_EXIT;
 }
-
-/* Enable themes in Windows XP and later */
-#pragma comment(linker,"\"/manifestdependency:type='win32' \
-name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
-processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
--- Sources/Tools/MaxMain/main.cpp
+++ Sources/Tools/MaxMain/main.cpp
@@ -256,7 +256,3 @@ ReferenceTarget *plGeneralAttrib::Clone(RemapDir &remap)
 // TEMP
 //////////////////////////////////////////////////////////////////////////////////
 
-/* Enable themes in Windows XP and later */
-#pragma comment(linker,"\"/manifestdependency:type='win32' \
-name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
-processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
