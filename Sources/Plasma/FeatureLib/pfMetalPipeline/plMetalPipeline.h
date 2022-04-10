5ba6de47cd5579ae7748c47b0adf709c174cde72
Darryl Pogue <darryl@dpogue.ca>
2022-04-09 19:47:55 -0700

    Move IClearShadowSlaves to pl3DPipeline

--- Sources/Plasma/FeatureLib/pfMetalPipeline/plMetalPipeline.h
+++ Sources/Plasma/FeatureLib/pfMetalPipeline/plMetalPipeline.h
@@ -185,7 +185,6 @@ private:
     void IPreprocessAvatarTextures();
     void IDrawClothingQuad(float x, float y, float w, float h,
                            float uOff, float vOff, plMipmap* tex);
-    void IClearShadowSlaves();
 
     void ICreateDeviceObjects();
     void IReleaseDynDeviceObjects();
