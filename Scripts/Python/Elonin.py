ec6631bf33fcac2289162d05839465120d3adc1c
ametist <ametist365@gmail.com>
2022-04-28 12:46:23 -0400

    Add Elonin
    Adds script files for Elonin cavern location by Dulcamara and maintained by ametist

---
+++ Scripts/Python/Elonin.py
@@ -0,0 +1,8 @@
+from Plasma import *
+from PlasmaTypes import *
+ 
+class Elonin(ptResponder):
+    def __init__(self):
+        ptResponder.__init__(self)
+        self.id = -1
+        self.version = 0
