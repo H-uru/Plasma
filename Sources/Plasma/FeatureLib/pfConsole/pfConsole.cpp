46ce8f58615726dd0ecad19d7738aac87eb2f015
Adam Johnson <AdamJohnso@gmail.com>
2022-05-14 11:33:20 -0400

    Apply suggestion from CR.
    Co-authored-by: ZarothYe <94947971+ZarothYe@users.noreply.github.com>

--- Sources/Plasma/FeatureLib/pfConsole/pfConsole.cpp
+++ Sources/Plasma/FeatureLib/pfConsole/pfConsole.cpp
@@ -557,8 +557,6 @@ void    pfConsole::IHandleKey( plKeyEventMsg *msg )
     else if (msg->GetCtrlKeyDown() && msg->GetKeyCode() == KEY_V)
     {
         ST::string text = plClipboard::GetInstance().GetClipboardText();
-        if (text.empty())
-            return;
 
         // Chop off leading or trailing newlines, which are probably not intended.
         text = text.trim("\r\n");
@@ -630,9 +628,11 @@ void    pfConsole::IHandleKey( plKeyEventMsg *msg )
         {
             // FIXME: make the console unicode friendly.
             IHandleCharacter((char)key);
-            findAgain = false;
-            findCounter = 0;
-            IUpdateTooltip();
+            if (strnlen(fWorkingLine, std::size(fWorkingLine)) < kMaxCharsWide - 2 && key != 0) {
+                findAgain = false;
+                findCounter = 0;
+                IUpdateTooltip();
+            }
         }
     }
 }
@@ -650,7 +650,7 @@ void    pfConsole::IHandleCharacter(const char c)
     size_t i = strnlen(fWorkingLine, std::size(fWorkingLine));
 
     // The current working line is too long. Ignore this character.
-    if (!(i < kMaxCharsWide - 2 && c != '\0'))
+    if (i >= kMaxCharsWide - 2 || c == '\0')
         return;
 
     // Advance any trailing characters one space to make room for the new character.
