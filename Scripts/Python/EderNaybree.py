2d95b16aee4ac579bbcf1c5da2813b7ec2e7f759
traveler263 <ctraveler69@yahoo.com>
2022-08-03 16:00:06 -0600

    the requested python id change
    there's got to be an easier way of doing this lol

--- Scripts/Python/EderNaybree.py
+++ Scripts/Python/EderNaybree.py
@@ -4,5 +4,5 @@ from PlasmaTypes import *
 class EderNaybree(ptResponder):
     def __init__(self):
         ptResponder.__init__(self)
-        self.id = -1
+        self.id = 81662042
         self.version = 0
