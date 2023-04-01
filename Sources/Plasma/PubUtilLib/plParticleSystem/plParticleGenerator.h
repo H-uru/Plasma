d579bb84d5375c94a48fa4a534fe1b293b3f3231
Darryl Pogue <darryl@dpogue.ca>
2023-04-01 15:34:23 -0700

    Resolve clang implicit conversion warning
    implicit conversion from 'int' to 'float' changes value from 2147483647 to 2147483648
    
    Co-Authored-By: Michael Hansen <zrax0111@gmail.com>

--- Sources/Plasma/PubUtilLib/plParticleSystem/plParticleGenerator.h
+++ Sources/Plasma/PubUtilLib/plParticleSystem/plParticleGenerator.h
@@ -67,7 +67,7 @@ public:
 
     static void ComputeDirection(float pitch, float yaw, hsVector3 &direction);
     static void ComputePitchYaw(float &pitch, float &yaw, const hsVector3 &dir);
-    static inline float GetRandomVar() { return 2.0f * rand() / RAND_MAX - 1; } // returns a num between +/- 1.0
+    static inline float GetRandomVar() { return 2.0f * rand() / float(RAND_MAX) - 1.0f; } // returns a num between +/- 1.0
 };
 
 class plSimpleParticleGenerator : public plParticleGenerator
