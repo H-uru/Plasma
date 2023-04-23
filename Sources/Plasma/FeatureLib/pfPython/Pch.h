82bf50ca5481eb753d7c3e7659beed3011e24a9e
dgelessus <dgelessus@users.noreply.github.com>
2023-04-23 23:18:09 +0200

    Remove no longer used includes from pfPython/Pch.h

--- Sources/Plasma/FeatureLib/pfPython/Pch.h
+++ Sources/Plasma/FeatureLib/pfPython/Pch.h
@@ -49,18 +49,12 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
  */
 
 // Standard Library Includes
-#include <algorithm>
 #include <array>
-#include <exception>
+#include <ctime>
 #include <functional>
-#include <locale>
 #include <memory>
 
-// Platform Library Includes
-#include <ctime>
-#include <sys/types.h>
-#include <sys/stat.h>
-
+// string_theory Library Includes
 #include <string_theory/string>
 #include <string_theory/string_stream>
 #include <string_theory/format>
