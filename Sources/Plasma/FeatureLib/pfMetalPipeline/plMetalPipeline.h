7ed57b059aee0ba777ce4a2a46dada8b5756d871
Darryl Pogue <darryl@dpogue.ca>
2023-05-04 22:23:02 -0700

    plTextFont rendering for GL Pipeline
    This enables the loading bars and console.

--- Sources/Plasma/FeatureLib/pfMetalPipeline/plMetalPipeline.h
+++ Sources/Plasma/FeatureLib/pfMetalPipeline/plMetalPipeline.h
@@ -240,8 +240,6 @@ private:
 
     static plMetalEnumerate enumerator;
 
-    plTextFont* fTextFontRefList;
-
     NS::AutoreleasePool* fCurrentPool;
 
     /// Describes the state for the "fixed function" shader.
