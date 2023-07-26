aefdc6eb8062d74271dc72e401d2d32959086958
dgelessus <dgelessus@users.noreply.github.com>
2023-07-26 20:37:14 +0200

    Document SDL read/write options in more detail

--- Sources/Plasma/PubUtilLib/plSDL/plStateVariable.cpp
+++ Sources/Plasma/PubUtilLib/plSDL/plStateVariable.cpp
@@ -101,6 +101,7 @@ public:
 // plStateVarNotificationInfo
 /////////////////////////////////////////////////////
 
+// Options: kSkipNotificationInfo
 void plStateVarNotificationInfo::Read(hsStream* s, uint32_t readOptions)
 {
     (void)s->ReadByte();  // unused: saveFlags
@@ -109,6 +110,7 @@ void plStateVarNotificationInfo::Read(hsStream* s, uint32_t readOptions)
         fHintString = hint;
 }
 
+// Options: (none)
 void plStateVarNotificationInfo::Write(hsStream* s, uint32_t writeOptions) const
 {
     (void)s->WriteByte(uint8_t(0));   // unused: saveFlags
@@ -118,6 +120,7 @@ void plStateVarNotificationInfo::Write(hsStream* s, uint32_t writeOptions) const
 /////////////////////////////////////////////////////
 // plStateVariable
 /////////////////////////////////////////////////////
+// Options: kSkipNotificationInfo, kTimeStampOnRead, kKeepDirty, kMakeDirty, kDirtyNonDefaults, [plSDStateVariable only: kForceConvert]
 bool plStateVariable::ReadData(hsStream* s, float timeConvert, uint32_t readOptions)
 {
     uint8_t saveFlags = s->ReadByte();
@@ -128,6 +131,7 @@ bool plStateVariable::ReadData(hsStream* s, float timeConvert, uint32_t readOpti
     return true;
 }
 
+// Options: kSkipNotificationInfo, [plSDStateVariable only: kDirtyOnly], [plSimpleStateVariable only: kWriteTimeStamps, kTimeStampOnRead, kTimeStampOnWrite, kDontWriteDirtyFlag, kMakeDirty, kDirtyNonDefaults]
 bool plStateVariable::WriteData(hsStream* s, float timeConvert, uint32_t writeOptions) const
 {
     bool writeNotificationInfo = ((writeOptions & plSDL::kSkipNotificationInfo)==0);
@@ -1771,6 +1775,7 @@ ST::string plSimpleStateVariable::GetKeyName(int idx) const
 #ifdef _MSC_VER
 #   pragma optimize( "g", off )    // disable float optimizations
 #endif
+// Options: (none)
 bool plSimpleStateVariable::IWriteData(hsStream* s, float timeConvert, int idx, uint32_t writeOptions) const
 {
 #ifdef HS_DEBUGGING
@@ -1854,6 +1859,7 @@ bool plSimpleStateVariable::IWriteData(hsStream* s, float timeConvert, int idx, 
     return true;
 }
 
+// Options: (none)
 bool plSimpleStateVariable::IReadData(hsStream* s, float timeConvert, int idx, uint32_t readOptions) 
 {   
     int j=idx*fVar.GetAtomicCount();
@@ -1941,6 +1947,7 @@ bool plSimpleStateVariable::IReadData(hsStream* s, float timeConvert, int idx, u
 #   pragma optimize( "", on )  // restore optimizations to their defaults
 #endif
 
+// Options: kSkipNotificationInfo, kWriteTimeStamps, kTimeStampOnRead, kTimeStampOnWrite, kDontWriteDirtyFlag, kMakeDirty, kDirtyNonDefaults
 bool plSimpleStateVariable::WriteData(hsStream* s, float timeConvert, uint32_t writeOptions) const
 {
 #ifdef HS_DEBUGGING
@@ -2012,6 +2019,7 @@ bool plSimpleStateVariable::WriteData(hsStream* s, float timeConvert, uint32_t w
 }
 
 // assumes var is created from the right type of descriptor (count, type, etc.)
+// Options: kSkipNotificationInfo, kTimeStampOnRead, kKeepDirty, kMakeDirty, kDirtyNonDefaults
 bool plSimpleStateVariable::ReadData(hsStream* s, float timeConvert, uint32_t readOptions)
 {
     // read base class data
@@ -2083,6 +2091,7 @@ bool plSimpleStateVariable::ReadData(hsStream* s, float timeConvert, uint32_t re
     return true;
 }
 
+// Options: all except for kDirtyOnly, kBroadcast, kForceConvert
 void plSimpleStateVariable::CopyData(const plSimpleStateVariable* other, uint32_t writeOptions/*=0*/)
 {
     // use stream as a medium
@@ -2478,6 +2487,7 @@ void plSDStateVariable::IDeInit()
 //
 // Make 'this' into a copy of 'other'.
 //
+// Options: all except for kDirtyOnly, kBroadcast, kForceConvert
 void plSDStateVariable::CopyFrom(plSDStateVariable* other, uint32_t writeOptions/*=0*/)
 {
     // IDeInit();
@@ -2492,6 +2502,7 @@ void plSDStateVariable::CopyFrom(plSDStateVariable* other, uint32_t writeOptions
 // copy them to my corresponding item.
 // Requires that records have the same descriptor.
 //
+// Options: all except for kBroadcast, kForceConvert
 void plSDStateVariable::UpdateFrom(plSDStateVariable* other, uint32_t writeOptions/*=0*/)
 {
     hsAssert(!other->GetSDVarDescriptor()->GetName().compare_i(fVarDescriptor->GetName()),
@@ -2593,6 +2604,7 @@ void plSDStateVariable::GetDirtyDataRecords(ConstDataRecList* recList) const
 //
 // read all SDVars
 //
+// Options: kSkipNotificationInfo, kTimeStampOnRead, kKeepDirty, kMakeDirty, kDirtyNonDefaults, kForceConvert
 bool plSDStateVariable::ReadData(hsStream* s, float timeConvert, uint32_t readOptions)
 {
     plStateVariable::ReadData(s, timeConvert, readOptions);
@@ -2636,6 +2648,7 @@ bool plSDStateVariable::ReadData(hsStream* s, float timeConvert, uint32_t readOp
 //
 // write all SDVars
 //
+// Options: kSkipNotificationInfo, kDirtyOnly
 bool plSDStateVariable::WriteData(hsStream* s, float timeConvert, uint32_t writeOptions) const
 {   
     plStateVariable::WriteData(s, timeConvert, writeOptions);
