4d3072568059ac264a8c4bba03eb77f5135de62d
Darryl Pogue <darryl@dpogue.ca>
2023-07-31 23:01:46 -0700

    Add macOS vcpkg overlay triplets
    Co-Authored-By: Colin Cornaby <colin.cornaby@mac.com>
    Co-Authored-By: Adam Johnson <AdamJohnso@gmail.com>

---
+++ Scripts/Triplets/arm64-macos-plasma.cmake
@@ -0,0 +1,14 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_DISABLE_COMPILER_TRACKING TRUE)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES arm64)
+set(VCPKG_OSX_DEPLOYMENT_TARGET "10.14")
+
+# This is a terrible hack because meson seems to suck.
+if(PORT STREQUAL cairo)
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+    set(VCPKG_BUILD_TYPE release)
+endif()
