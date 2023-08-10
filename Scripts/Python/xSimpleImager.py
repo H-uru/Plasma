471494a4f97114a765f80f3b4c228a997fb92ce9
Edmond Mondor <Hazado22@hotmail.com>
2023-08-10 11:25:40 -0700

    add pellet upload check
    Sets up the an SDL variable so you can set individual pellet uploads to everyone/member only/off

--- Scripts/Python/xSimpleImager.py
+++ Scripts/Python/xSimpleImager.py
@@ -77,6 +77,7 @@ ImagerPelletUpload = ptAttribBoolean(10, "Pellet Score Imager?", 0)
 ImagerClueObject = ptAttribSceneobject(11, "Imager Object (for puzzle clue)")
 ImagerClueTime = ptAttribInt(12, "Number of seconds until clue image shows",default=870)
 ImagerRandomTime = ptAttribInt(13, "Random number added to make timer more variable",default=0)
+ImagerPelletCheckVariable = ptAttribString(14,"Pellet upload SDL variable (optional)")
 #----------
 # globals
 #----------
@@ -298,7 +299,7 @@ class xSimpleImager(ptModifier):
                         for event in events:
                             if event[0] == kCollisionEvent:
                                 kiLevel = PtDetermineKILevel()
-                                if (kiLevel < kNormalKI):
+                                if (kiLevel < kNormalKI or not self.PelletUploadCheck()):
                                     return
                                 if ImagerPelletUpload.value:
                                     messagetoki = str(ImagerName.value) + "<p>"
@@ -521,6 +522,19 @@ class xSimpleImager(ptModifier):
                 ageVault.setDeviceInbox(ImagerName.value, ImagerName.value, self, kSettingDeviceInbox)
             else:
                 ageVault.setDeviceInbox(ImagerName.value, ageSDL[ImagerInboxVariable.value][0], self, kSettingDeviceInbox)
+
+    def PelletUploadCheck(self):
+        """Age SDL Check to prevent unwanted pellet drops"""
+        ageSDL = PtGetAgeSDL()
+        pelletCheck = 0
+        if ImagerPelletCheckVariable.value:
+            pelletCheck = ageSDL[ImagerPelletCheckVariable.value][0]
+        if pelletCheck >= 2: # Locked for everyone
+            return False
+        elif pelletCheck == 1 and ptVault().amOwnerOfCurrentAge(): #Hood members only
+            return True
+        elif pelletCheck <= 0: # Anyone
+            return True
 
     def OnBackdoorMsg(self, target, param):
         if target == "imager" and param == "refresh" and ImagerName.value == "D'ni  Imager Right":
