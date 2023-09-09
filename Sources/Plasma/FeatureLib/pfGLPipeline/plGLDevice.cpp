0c4e1961600612e1a9c4bcc5af3f022e3cc3b179
Darryl Pogue <darryl@dpogue.ca>
2023-09-09 02:53:41 -0700

    Reorganize platform GL context providers

--- Sources/Plasma/FeatureLib/pfGLPipeline/plGLDevice.cpp
+++ Sources/Plasma/FeatureLib/pfGLPipeline/plGLDevice.cpp
@@ -47,262 +47,15 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 #include "plGLDevice.h"
 #include "plGLPipeline.h"
 
+#include "plCGLDevice.h"
+#include "plEGLDevice.h"
+#include "plWGLDevice.h"
+
 #include "plDrawable/plGBufferGroup.h"
 #include "plGImage/plMipmap.h"
 #include "plGImage/plCubicEnvironmap.h"
 #include "plPipeline/plRenderTarget.h"
 #include "plStatusLog/plStatusLog.h"
-
-#pragma region EGL_Init
-#ifdef USE_EGL
-#include <epoxy/egl.h>
-
-void InitEGLDevice(plGLDevice* dev)
-{
-    EGLNativeDisplayType device = static_cast<EGLNativeDisplayType>((void*)dev->fDevice);
-    EGLDisplay display = EGL_NO_DISPLAY;
-    EGLContext context = EGL_NO_CONTEXT;
-    EGLSurface surface = EGL_NO_SURFACE;
-
-    do {
-        if (!eglBindAPI(EGL_OPENGL_API)) {
-            dev->fErrorMsg = "Could not bind to OpenGL API";
-            break;
-        }
-
-        /* Set up the display */
-        display = eglGetDisplay(device);
-        if (display == EGL_NO_DISPLAY) {
-            dev->fErrorMsg = "Could not get the display";
-            break;
-        }
-
-        if (!eglInitialize(display, nullptr, nullptr)) {
-            dev->fErrorMsg = "Could not initialize the display";
-            break;
-        }
-
-        /* Set up the config attributes for EGL */
-        EGLConfig config;
-        EGLint config_count;
-        EGLint config_attrs[] = {
-            EGL_BUFFER_SIZE, 24,
-            EGL_DEPTH_SIZE, 24,
-            EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
-            EGL_NONE
-        };
-
-        if (!eglChooseConfig(display, config_attrs, &config, 1, &config_count) || config_count != 1) {
-            dev->fErrorMsg = "Could not choose appropriate config";
-            break;
-        }
-
-        /* Set up the GL context */
-        EGLint ctx_attrs[] = {
-            EGL_CONTEXT_MAJOR_VERSION, 3,
-            EGL_CONTEXT_OPENGL_PROFILE_MASK, EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT,
-            EGL_NONE
-        };
-
-        context = eglCreateContext(display, config, EGL_NO_CONTEXT, ctx_attrs);
-        if (context == EGL_NO_CONTEXT) {
-            dev->fErrorMsg = "Unable to create rendering context";
-            break;
-        }
-
-        /* Set up the rendering surface */
-        surface = eglCreateWindowSurface(display, config, (EGLNativeWindowType)dev->fWindow, nullptr);
-        if (surface == EGL_NO_SURFACE) {
-            dev->fErrorMsg = "Unable to create rendering surface";
-            break;
-        }
-
-        /* Associate everything */
-        if (eglMakeCurrent(display, surface, surface, context) == EGL_FALSE) {
-            dev->fErrorMsg = "Failed to attach EGL context to surface";
-            break;
-        }
-
-        // Successfully initialized:
-        dev->fDisplay = display;
-        dev->fContext = context;
-        dev->fSurface = surface;
-        dev->fContextType = plGLDevice::kEGL;
-        return;
-    } while (0);
-
-    // Cleanup for failure case:
-    if (surface != EGL_NO_SURFACE) {
-        eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-        eglDestroySurface(display, surface);
-    }
-
-    if (context != EGL_NO_CONTEXT)
-        eglDestroyContext(display, context);
-
-    if (display != EGL_NO_DISPLAY)
-        eglTerminate(display);
-}
-
-void FiniEGLDevice(plGLDevice* dev)
-{
-    EGLSurface surface = static_cast<EGLSurface>(dev->fSurface);
-    EGLContext context = static_cast<EGLContext>(dev->fContext);
-    EGLDisplay display = static_cast<EGLDisplay>(dev->fDisplay);
-
-    if (surface != EGL_NO_SURFACE)
-        eglDestroySurface(display, surface);
-
-    if (context != EGL_NO_CONTEXT)
-        eglDestroyContext(display, context);
-
-    if (display != EGL_NO_DISPLAY)
-        eglTerminate(display);
-}
-#endif // USE_EGL
-#pragma endregion EGL_Init
-
-#pragma region WGL_Init
-#ifdef HS_BUILD_FOR_WIN32
-#include "hsWindows.h"
-#include <epoxy/wgl.h>
-
-void InitWGLDevice(plGLDevice* dev)
-{
-    HDC dc = static_cast<HDC>((HANDLE)dev->fDevice);
-    HGLRC ctx = nullptr;
-
-    do {
-        PIXELFORMATDESCRIPTOR pfd{};
-        pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
-        pfd.nVersion = 1;
-        pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_GENERIC_ACCELERATED | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
-        pfd.iPixelType = PFD_TYPE_RGBA;
-        pfd.cColorBits = 24;
-        pfd.cDepthBits = 24;
-        pfd.iLayerType = PFD_MAIN_PLANE;
-
-        int format = ChoosePixelFormat(dc, &pfd);
-        if (!format) {
-            dev->fErrorMsg = "Could not find appropriate pixel config";
-            break;
-        }
-
-        if (!SetPixelFormat(dc, format, &pfd)) {
-            dev->fErrorMsg = "Could not set appropriate pixel config";
-            break;
-        }
-
-        ctx = wglCreateContext(dc);
-        if (!ctx) {
-            dev->fErrorMsg = "Unable to create rendering context";
-            break;
-        }
-
-        if (!wglMakeCurrent(dc, ctx)) {
-            dev->fErrorMsg = "Failed to attach WGL context to surface";
-            break;
-        }
-
-        // Successfully initialized:
-        dev->fContext = ctx;
-        dev->fContextType = plGLDevice::kWGL;
-        return;
-    } while (0);
-
-    // Cleanup for failure case:
-    if (ctx) {
-        wglMakeCurrent(nullptr, nullptr);
-        wglDeleteContext(ctx);
-    }
-}
-
-void FiniWGLDevice(plGLDevice* dev)
-{
-    HGLRC ctx = static_cast<HGLRC>(dev->fContext);
-
-    if (ctx) {
-        wglMakeCurrent(nullptr, nullptr);
-        wglDeleteContext(ctx);
-    }
-}
-#endif // HS_BUILD_FOR_WIN32
-#pragma endregion WGL_Init
-
-#pragma region CGL_Init
-#ifdef HS_BUILD_FOR_MACOS
-#include <AvailabilityMacros.h>
-#include <OpenGL/OpenGL.h>
-void InitCGLDevice(plGLDevice* dev)
-{
-    IGNORE_WARNINGS_BEGIN("deprecated-declarations")
-
-    CGLPixelFormatObj pix = nullptr;
-    CGLContextObj ctx = nullptr;
-
-    do {
-        CGLPixelFormatAttribute attribs[6] = {
-            kCGLPFAAccelerated,
-            kCGLPFANoRecovery,
-            kCGLPFADoubleBuffer,
-#if MAC_OS_X_VERSION_MIN_REQUIRED >= 1070
-            // OpenGL profiles introduced in 10.7
-            kCGLPFAOpenGLProfile, (CGLPixelFormatAttribute) kCGLOGLPVersion_3_2_Core,
-#endif
-            (CGLPixelFormatAttribute) 0
-        };
-
-        int nPix = 0;
-        if (CGLChoosePixelFormat(attribs, &pix, &nPix) != kCGLNoError || nPix == 0) {
-            dev->fErrorMsg = "Could not choose appropriate config";
-            break;
-        }
-
-        if (CGLCreateContext(pix, nullptr, &ctx) != kCGLNoError) {
-            dev->fErrorMsg = "Unable to create rendering context";
-            break;
-        }
-
-        if (CGLSetCurrentContext(ctx) != kCGLNoError) {
-            dev->fErrorMsg = "Failed to attach CGL context to surface";
-            break;
-        }
-
-        CGLReleasePixelFormat(pix);
-
-        // Successfully initialized:
-        dev->fContext = ctx;
-        dev->fContextType = plGLDevice::kCGL;
-        return;
-    } while (0);
-
-    // Cleanup for failure case:
-    if (ctx) {
-        CGLSetCurrentContext(nullptr);
-        CGLReleaseContext(ctx);
-    }
-
-    if (pix)
-        CGLReleasePixelFormat(pix);
-
-    IGNORE_WARNINGS_END
-}
-
-void FiniCGLDevice(plGLDevice* dev)
-{
-    IGNORE_WARNINGS_BEGIN("deprecated-declarations")
-
-    CGLContextObj ctx = static_cast<CGLContextObj>(dev->fContext);
-
-    if (ctx) {
-        CGLSetCurrentContext(nullptr);
-        CGLReleaseContext(ctx);
-    }
-
-    IGNORE_WARNINGS_END
-}
-#endif // HS_BUILD_FOR_MACOS
-#pragma endregion CGL_Init
 
 #ifdef HS_DEBUGGING
 static void GLAPIENTRY plGLDebugLog(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
@@ -329,8 +82,7 @@ GLfloat* hsMatrix2GL(const hsMatrix44& src, GLfloat* dst)
 }
 
 plGLDevice::plGLDevice()
-    : fErrorMsg(), fPipeline(), fContextType(kNone), fWindow(), fDevice(),
-    fDisplay(), fSurface(), fContext(), fActiveThread(), fCurrentProgram()
+    : fErrorMsg(), fPipeline(), fImpl(), fWindow(), fDevice(), fActiveThread(), fCurrentProgram()
 {
     memcpy(fMatrixL2W, kIdentityMatrix, sizeof(GLfloat) * 16);
     memcpy(fMatrixW2C, kIdentityMatrix, sizeof(GLfloat) * 16);
@@ -348,23 +100,23 @@ bool plGLDevice::InitDevice()
     // ANGLE.
     //
     // On Linux, this should be true with mesa or nvidia drivers.
-    if (epoxy_has_egl() && fContextType == kNone)
-        InitEGLDevice(this);
+    if (epoxy_has_egl() && !fImpl)
+        fImpl = plEGLDevice::TryInit(fWindow, fDevice, fErrorMsg);
 #endif
 
 #ifdef HS_BUILD_FOR_WIN32
-    if (fContextType == kNone)
-        InitWGLDevice(this);
+    if (!fImpl)
+        fImpl = plWGLDevice::TryInit(fWindow, fDevice, fErrorMsg);
 #endif
 
 #ifdef HS_BUILD_FOR_MACOS
-    if (fContextType == kNone)
-        InitCGLDevice(this);
+    if (!fImpl)
+        fImpl = plCGLDevice::TryInit(fWindow, fDevice, fErrorMsg);
 #endif
 
     // If we still don't have a valid context type set by this point, we've
     // failed to initialize so we need to exit.
-    if (fContextType == kNone)
+    if (!fImpl)
         return false;
 
     plStatusLog::AddLineSF("pipeline.log", "Initialized with OpenGL {}", reinterpret_cast<const char*>(glGetString(GL_VERSION)));
@@ -396,20 +148,8 @@ bool plGLDevice::InitDevice()
 
 void plGLDevice::Shutdown()
 {
-#ifdef USE_EGL
-    if (fContextType == kEGL) {
-        FiniEGLDevice(this);
-        return;
-    }
-#endif
-
-#ifdef HS_BUILD_FOR_WIN32
-    FiniWGLDevice(this);
-#endif
-
-#ifdef HS_BUILD_FOR_MACOS
-    FiniCGLDevice(this);
-#endif
+    if (fImpl)
+        fImpl->Shutdown();
 }
 
 void plGLDevice::SetRenderTarget(plRenderTarget* target)
@@ -452,12 +192,6 @@ void plGLDevice::SetViewport()
 
 bool plGLDevice::BeginRender()
 {
-#ifdef HS_BUILD_FOR_WIN32
-    // Best practice, apparently, is to get and release the DC every time we need it.
-    // A DC is only valid on one thread at a time.
-    fDevice = static_cast<hsWindowHndl>((HANDLE)GetDC(fWindow));
-#endif
-
     if (fActiveThread == hsThread::ThisThreadHash()) {
         return true;
     }
@@ -470,20 +204,10 @@ bool plGLDevice::BeginRender()
         return false;
     }
 
-#ifdef USE_EGL
-    if (fContextType == kEGL) {
-        EGLDisplay display = static_cast<EGLDisplay>(fDisplay);
-        EGLContext context = static_cast<EGLContext>(fContext);
-        EGLSurface surface = static_cast<EGLSurface>(fSurface);
+    if (fImpl)
+        return fImpl->BeginRender(fErrorMsg);
 
-        if (eglMakeCurrent(display, surface, surface, context) == EGL_FALSE) {
-            fErrorMsg = "Failed to attach EGL context to surface";
-            return false;
-        }
-    } //else
-#endif
-
-    return true;
+    return false;
 }
 
 bool plGLDevice::EndRender()
@@ -492,24 +216,8 @@ bool plGLDevice::EndRender()
         return true;
     }
 
-#ifdef USE_EGL
-    if (fContextType == kEGL) {
-        if (eglSwapBuffers(static_cast<EGLDisplay>(fDisplay), static_cast<EGLSurface>(fSurface)) == EGL_FALSE) {
-            fErrorMsg = "Failed to swap buffers";
-            return false;
-        }
-        return true;
-    } else
-#endif
-
-#ifdef HS_BUILD_FOR_WIN32
-    if (fContextType == kWGL) {
-        SwapBuffers(static_cast<HDC>((HANDLE)fDevice));
-    }
-
-    ReleaseDC(fWindow, static_cast<HDC>((HANDLE)fDevice));
-    fDevice = nullptr;
-#endif
+    if (fImpl)
+        return fImpl->EndRender(fErrorMsg);
 
     return false;
 }
