29da02365b44b51a2b33c4fee05253fd4225cdb1
noah the goodra <peterpan0413@live.com>
2023-11-01 04:58:53 -0500

    move the list of shaders out and make it a constexpr

--- Sources/Tools/plShaderAssembler/main.cpp
+++ Sources/Tools/plShaderAssembler/main.cpp
@@ -162,64 +162,51 @@ static void IAssShader(const plDXShaderAssembler& ass, const char* name)
         fputs("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n", stderr);
     }
 }
-
-int main(int argc, char* argv[])
-{
-    if( argc < 2 )
-    {
-        ST::printf("{} <file0> <file1> ...\n", argv[0]);
-        return 0;
-    }
-
-    const char* const * nameList = nullptr;
-    int numNames = 0;
-    if (stricmp(argv[1], "all") == 0)
-    {
-        // Just copy in the enum and use Replace on
-        // vs_ => "vs_
-        // ps_ => "ps_
-        // , => ",
-        static const char* kEnumNames[] = {
-            "vs_WaveFixedFin6",
-            "ps_WaveFixed",
-            "vs_CompCosines",
-            "ps_CompCosines",
-            "vs_ShoreLeave6",
-            "ps_ShoreLeave6",
-            "vs_WaveRip",
-            "ps_WaveRip",
-            "vs_WaveDec1Lay",
-            "vs_WaveDec2Lay11",
-            "vs_WaveDec2Lay12",
-            "vs_WaveDecEnv",
-            "ps_CbaseAbase",
-            "ps_CalphaAbase",
-            "ps_CalphaAMult",
-            "ps_CalphaAadd",
-            "ps_CaddAbase",
-            "ps_CaddAMult",
-            "ps_CaddAAdd",
-            "ps_CmultAbase",
-            "ps_CmultAMult",
-            "ps_CmultAAdd",
-            "ps_WaveDecEnv",
-            "vs_WaveGraph2",
-            "ps_WaveGraph",
-            "vs_WaveGridFin",
-            "ps_WaveGrid",
-            "vs_BiasNormals",
-            "ps_BiasNormals",
-            "vs_ShoreLeave7",
-            "vs_WaveRip7",
-            "ps_MoreCosines",
-            "vs_WaveDec1Lay_7",
-            "vs_WaveDec2Lay11_7",
-            "vs_WaveDec2Lay12_7",
-            "vs_WaveDecEnv_7",
-            "vs_WaveFixedFin7",
-            "vs_GrassShader",
-            "ps_GrassShader"
-        };
+// Just copy in the enum and use Replace on
+// vs_ => "vs_
+// ps_ => "ps_
+// , => ",
+static constexpr std::string_view kEnumNames[] = {
+    "vs_WaveFixedFin6",
+    "ps_WaveFixed",
+    "vs_CompCosines",
+    "ps_CompCosines",
+    "vs_ShoreLeave6",
+    "ps_ShoreLeave6",
+    "vs_WaveRip",
+    "ps_WaveRip",
+    "vs_WaveDec1Lay",
+    "vs_WaveDec2Lay11",
+    "vs_WaveDec2Lay12",
+    "vs_WaveDecEnv",
+    "ps_CbaseAbase",
+    "ps_CalphaAbase",
+    "ps_CalphaAMult",
+    "ps_CalphaAadd",
+    "ps_CaddAbase",
+    "ps_CaddAMult",
+    "ps_CaddAAdd",
+    "ps_CmultAbase",
+    "ps_CmultAMult",
+    "ps_CmultAAdd",
+    "ps_WaveDecEnv",
+    "vs_WaveGraph2",
+    "ps_WaveGraph",
+    "vs_WaveGridFin",
+    "ps_WaveGrid",
+    "vs_BiasNormals",
+    "ps_BiasNormals",
+    "vs_ShoreLeave7",
+    "vs_WaveRip7",
+    "ps_MoreCosines",
+    "vs_WaveDec1Lay_7",
+    "vs_WaveDec2Lay11_7",
+    "vs_WaveDec2Lay12_7",
+    "vs_WaveDecEnv_7",
+    "vs_WaveFixedFin7",
+    "vs_GrassShader",
+    "ps_GrassShader"
+};
 
         nameList = kEnumNames;
         numNames = std::size(kEnumNames);
