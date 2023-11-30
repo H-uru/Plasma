bd6718a69f452102e171f1dcccb832a9c33693a6
noah the goodra <peterpan0413@live.com>
2023-11-29 20:58:14 -0600

    fix both slave and return result correctly

--- Sources/Tools/MaxMain/MaxCompat.h
+++ Sources/Tools/MaxMain/MaxCompat.h
@@ -293,7 +293,7 @@ public:
 #define XREFOBJ_COMPAT_CLASS_ID Class_ID(0x92aab38c, 0)
 // This definition is removed in later versions of the sdk
 // In newer versions of the sdk Progress returns an enum value of KeyReduceResult
-#if MAX_VERSION_MAJOR < 24
+#if MAX_VERSION_MAJOR <= 25
 #   define BIPDRIVEN_CONTROL_CLASS_ID BIPSLAVE_CONTROL_CLASS_ID
 #   define KeyReduceResult int
 #endif
