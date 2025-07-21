a0918618b2273e90a40c83f7ae6aa109a7ad0a1b
dgelessus <dgelessus@users.noreply.github.com>
2025-07-21 23:06:16 +0200

    Remove a few unused debug-conditional macros
    These are very easy to replicate with a manual #ifdef HS_DEBUGGING if
    needed.

--- Sources/Plasma/CoreLib/HeadSpin.h
+++ Sources/Plasma/CoreLib/HeadSpin.h
@@ -330,24 +330,20 @@ void DebugMsg(const char* fmt, ...);
 #ifdef HS_DEBUGGING
     
     void    hsDebugMessage(const char* message, long refcon);
-    #define hsDebugCode(code)                   code
     #define hsIfDebugMessage(expr, msg, ref)    (void)( (!!(expr)) || (hsDebugMessage(msg, ref), 0) )
     #define hsAssert(expr, ...)                 (void)( (!!(expr)) || (ErrorAssert(__LINE__, __FILE__, __VA_ARGS__), 0) )
     #define ASSERT(expr)                        (void)( (!!(expr)) || (ErrorAssert(__LINE__, __FILE__, #expr), 0) )
     #define ASSERTMSG(expr, ...)                (void)( (!!(expr)) || (ErrorAssert(__LINE__, __FILE__, __VA_ARGS__), 0) )
     #define FATAL(...)                          ErrorAssert(__LINE__, __FILE__, __VA_ARGS__)
-    #define DEBUG_BREAK_IF_DEBUGGER_PRESENT     DebugBreakIfDebuggerPresent
     
 #else   /* Not debugging */
 
     #define hsDebugMessage(message, refcon)     ((void)0)
-    #define hsDebugCode(code)                   /* empty */
     #define hsIfDebugMessage(expr, msg, ref)    ((void)0)
     #define hsAssert(expr, ...)                 ((void)0)
     #define ASSERT(expr)                        ((void)0)
     #define ASSERTMSG(expr, ...)                ((void)0)
     #define FATAL(...)                          ((void)0)
-    #define DEBUG_BREAK_IF_DEBUGGER_PRESENT()   ((void)0)
 
 #endif  // HS_DEBUGGING
 
