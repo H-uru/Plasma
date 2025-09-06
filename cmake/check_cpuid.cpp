8a15484c91850875ddc5a5661addf6c1354328eb
Colin Cornaby <colin.cornaby@mac.com>
2025-09-05 22:28:38 -0700

    Adding support for Windows/ARM64

--- cmake/check_cpuid.cpp
+++ cmake/check_cpuid.cpp
@@ -1,11 +1,16 @@
 #if defined(_MSC_VER) || ((defined(_WIN32) || defined(_WIN64)) && defined(__INTEL_COMPILER))
 #  include <intrin.h>
+#  define MSC_COMPATIBLE
 #elif defined(__GNUC__)
 #  include <cpuid.h>
 #endif
 
-/* Just needed to look for the headers -- this just makes the compiler happy. */
 int main(int argc, char *argv[])
 {
+#if defined(MSC_COMPATIBLE)
+    // We need to test - Windows ARM64 will have the headers but not the definition
+    unsigned int features[4] = {0, 0, 0, 0};
+    __cpuid(features, 0);
+#endif
     return 0;
 }
