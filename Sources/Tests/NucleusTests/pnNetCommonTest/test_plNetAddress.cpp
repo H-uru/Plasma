6e944e5ec715147d734b73c7b942b25f291e9cb7
dgelessus <dgelessus@users.noreply.github.com>
2025-09-09 17:19:45 +0200

    Add unit tests for plNetAddress to string conversions

---
+++ Sources/Tests/NucleusTests/pnNetCommonTest/test_plNetAddress.cpp
@@ -0,0 +1,72 @@
+/*==LICENSE==*
+
+CyanWorlds.com Engine - MMOG client, server and tools
+Copyright (C) 2011  Cyan Worlds, Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Additional permissions under GNU GPL version 3 section 7
+
+If you modify this Program, or any covered work, by linking or
+combining it with any of RAD Game Tools Bink SDK, Autodesk 3ds Max SDK,
+NVIDIA PhysX SDK, Microsoft DirectX SDK, OpenSSL library, Independent
+JPEG Group JPEG library, Microsoft Windows Media SDK, or Apple QuickTime SDK
+(or a modified version of those libraries),
+containing parts covered by the terms of the Bink SDK EULA, 3ds Max EULA,
+PhysX SDK EULA, DirectX SDK EULA, OpenSSL and SSLeay licenses, IJG
+JPEG Library README, Windows Media SDK EULA, or QuickTime SDK EULA, the
+licensors of this Program grant you additional
+permission to convey the resulting work. Corresponding Source for a
+non-source form of such a combination shall include the source code for
+the parts of OpenSSL and IJG JPEG Library used as well as that of the covered
+work.
+
+You can contact Cyan Worlds, Inc. by email legal@cyan.com
+ or by snail mail at:
+      Cyan Worlds, Inc.
+      14617 N Newport Hwy
+      Mead, WA   99021
+
+*==LICENSE==*/
+
+#include <array>
+#include <gtest/gtest.h>
+#include <string_theory/string>
+
+#include "pnNetCommon/plNetAddress.h"
+
+using namespace ST::literals;
+
+TEST(plNetAddress, HostPortAsString)
+{
+    plNetAddress addr1({1, 2, 3, 4}, 80);
+    EXPECT_EQ(addr1.GetHostString(), "1.2.3.4"_st);
+    EXPECT_EQ(addr1.GetHostWithPort(), "1.2.3.4:80"_st);
+
+    plNetAddress addr2({10, 20, 30, 40}, 443);
+    EXPECT_EQ(addr2.GetHostString(), "10.20.30.40"_st);
+    EXPECT_EQ(addr2.GetHostWithPort(), "10.20.30.40:443"_st);
+
+    plNetAddress addr3({127, 0, 0, 1}, 8080);
+    EXPECT_EQ(addr3.GetHostString(), "127.0.0.1"_st);
+    EXPECT_EQ(addr3.GetHostWithPort(), "127.0.0.1:8080"_st);
+
+    plNetAddress addr4({184, 73, 198, 22}, 14617);
+    EXPECT_EQ(addr4.GetHostString(), "184.73.198.22"_st);
+    EXPECT_EQ(addr4.GetHostWithPort(), "184.73.198.22:14617"_st);
+
+    plNetAddress addr5({255, 254, 253, 252}, 65535);
+    EXPECT_EQ(addr5.GetHostString(), "255.254.253.252"_st);
+    EXPECT_EQ(addr5.GetHostWithPort(), "255.254.253.252:65535"_st);
+}
