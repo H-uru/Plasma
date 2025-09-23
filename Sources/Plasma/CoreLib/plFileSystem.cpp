b953de562c9d04a1f05c75497c1ded9afa230c78
Darryl Pogue <darryl@dpogue.ca>
2025-09-23 16:19:31 -0700

    Fix improper use of `getcwd` function
    Behaviour is undefined if the buffer parameter is null.

--- Sources/Plasma/CoreLib/plFileSystem.cpp
+++ Sources/Plasma/CoreLib/plFileSystem.cpp
@@ -262,10 +262,10 @@ plFileName plFileSystem::GetCWD()
         cwd = ST::string::from_wchar(cwd_sm);
     }
 #else
-    char *cwd_a = getcwd(nullptr, 0);
+    char cwd_a[1024];
+    getcwd(cwd_a, sizeof(cwd_a));
     hsAssert(cwd_a, "Failure to get working directory (unsupported libc?)");
     cwd = cwd_a;
-    free(cwd_a);
 #endif
 
     return cwd;
