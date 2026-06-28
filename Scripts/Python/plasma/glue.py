6abb6650ea260bee816cfc64d24dd46d28ed15e0
dgelessus <dgelessus@users.noreply.github.com>
2026-06-28 03:53:23 +0200

    Fix odd formatting in glue.py

--- Scripts/Python/plasma/glue.py
+++ Scripts/Python/plasma/glue.py
@@ -39,24 +39,29 @@
 #       Mead, WA   99021
 #
 # *==LICENSE==*/
+
 # glue code in python for the glue code in C++
 # This assumes that this will be loaded into the module that we are trying to do
 # with an execfile('.\\python\\system\\glue.py') at the end of the module (after everything is defined)
 # SPECIAL WARNING(1): This glue code returns the attributes in reverse ID order!
+
 glue_cl = None       # the class of the modifier
 glue_inst = None     # instance of the class modifier
 glue_params = None   # parameters dictionary: mapped id to instance
 glue_paramKeys = None # this is the parameter ID list, that should be sorted
+
 try:
     x = glue_verbose
 except NameError:
     glue_verbose = 0
+
 def glue_getClass():
     global glue_cl
+
     if glue_cl == None:
         try:
             cl = globals()[glue_name]
-            if issubclass(cl,ptModifier):
+            if issubclass(cl, ptModifier):
                 glue_cl = cl
             else:
                 if glue_verbose:
@@ -67,32 +72,41 @@ def glue_getClass():
                     PtDebugPrint("Could not find class %s" % (glue_name))
                 except NameError:
                     PtDebugPrint("Filename/classname not set!")
+
     return glue_cl
+
 def glue_getInst():
     global glue_inst
+
     if glue_inst is None:
         cl = glue_getClass()
         if cl != None:
             glue_inst = cl()
+
     return glue_inst
+
 def glue_delInst():
     global glue_inst
     global glue_cl
     global glue_params
     global glue_paramKeys
+
     if glue_inst is not None:
         del glue_inst
+
     # remove our references
     glue_cl = None
     glue_params = None
     glue_paramKeys = None
+
 def glue_getVersion():
     inst = glue_getInst()
     ver = inst.version
     glue_delInst()
     return ver
+
 def glue_findAndAddAttribs(obj, glue_params):
-    if isinstance(obj,ptAttribute):
+    if isinstance(obj, ptAttribute):
         if obj.id in glue_params:
             if glue_verbose:
                 PtDebugPrint("WARNING: Duplicate attribute ids!")
@@ -108,10 +122,11 @@ def glue_findAndAddAttribs(obj, glue_params):
     elif isinstance(obj, tuple):
         for o in obj:
             glue_findAndAddAttribs(o, glue_params)
-            
+
 def glue_getParamDict():
     global glue_params
     global glue_paramKeys
+
     if glue_params is None:
         glue_params = {}
         gd = globals()
@@ -120,31 +135,43 @@ def glue_getParamDict():
         # rebuild the parameter sorted key list
         glue_paramKeys = list(glue_params.keys())
         glue_paramKeys.sort()
-        glue_paramKeys.reverse()    # reserve the order because PlasmaMax will ask for them in reverse order
+        glue_paramKeys.reverse() # reserve the order because PlasmaMax will ask for them in reverse order
+
     return glue_params
+
 def glue_getClassName():
     cl = glue_getClass()
     if cl != None:
         return cl.__name__
+
     if glue_verbose:
         PtDebugPrint("Class not found in %s.py" % (glue_name))
+
     return None
+
 def glue_getBlockID():
     inst = glue_getInst()
     if inst != None:
         return inst.id
+
     if glue_verbose:
         PtDebugPrint("Instance could not be created in %s.py" % (glue_name))
+
     return None
+
 def glue_getNumParams():
     pd = glue_getParamDict()
     if pd != None:
         return len(pd)
+
     if glue_verbose:
         PtDebugPrint("No attributes found in %s.py" % (glue_name))
+
     return 0
+
 def glue_getParam(number):
     global glue_paramKeys
+
     pd = glue_getParamDict()
     if pd != None:
         # see if there is a paramKey list
@@ -160,10 +187,13 @@ def glue_getParam(number):
             else:
                 if glue_verbose:
                     PtDebugPrint("glue_getParam: Error! %d out of range of attribute list" % (number))
+
     if glue_verbose:
         PtDebugPrint("GLUE: Attribute list error")
+
     return None
-def glue_setParam(id,value):
+
+def glue_setParam(id, value):
     pd = glue_getParamDict()
     if pd != None:
         if id in pd:
@@ -171,39 +201,45 @@ def glue_setParam(id,value):
             try:
                 pd[id].__setvalue__(value)
             except AttributeError:
-                if isinstance(pd[id],ptAttributeList):
+                if isinstance(pd[id], ptAttributeList):
                     try:
                         if not isinstance(pd[id].value, list):
-                            pd[id].value = []   # make sure that the value starts as an empty list
+                            pd[id].value = [] # make sure that the value starts as an empty list
                     except AttributeError:
-                        pd[id].value = []   # or if value hasn't been defined yet, then do it now
-                    pd[id].value.append(value)   # add in new value to list
+                        pd[id].value = [] # or if value hasn't been defined yet, then do it now
+
+                    pd[id].value.append(value) # add in new value to list
                 else:
                     pd[id].value = value
         else:
             if glue_verbose:
-                PtDebugPrint("setParam: can't find id=",id)
+                PtDebugPrint("setParam: can't find id=", id)
     else:
         PtDebugPrint("setParma: Something terribly has gone wrong. Head for the cover.")
+
 def glue_isNamedAttribute(id):
     pd = glue_getParamDict()
     if pd != None:
         try:
-            if isinstance(pd[id],ptAttribNamedActivator):
+            if isinstance(pd[id], ptAttribNamedActivator):
                 return 1
-            if isinstance(pd[id],ptAttribNamedResponder):
+            if isinstance(pd[id], ptAttribNamedResponder):
                 return 2
         except KeyError:
             if glue_verbose:
                 PtDebugPrint("Could not find id=%d attribute" % (id))
+
     return 0
+
 def glue_isMultiModifier():
     inst = glue_getInst()
-    if isinstance(inst,ptMultiModifier):
+    if isinstance(inst, ptMultiModifier):
         return 1
     return 0
+
 def glue_getVisInfo(number):
     global glue_paramKeys
+
     pd = glue_getParamDict()
     if pd != None:
         # see if there is a paramKey list
@@ -219,6 +255,8 @@ def glue_getVisInfo(number):
             else:
                 if glue_verbose:
                     PtDebugPrint("glue_getVisInfo: Error! %d out of range of attribute list" % (number))
+
     if glue_verbose:
         PtDebugPrint("GLUE: Attribute list error")
+
     return None
